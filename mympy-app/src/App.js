6924a3e4b5b5a81e3a7b89ccb70a1187f0a6fc34
Krisli Dimo <krislidimo@gmail.com>
2019-07-12 09:55:12 -0500

    added axios

--- mympy-app/src/App.js
+++ mympy-app/src/App.js
@@ -1,12 +1,24 @@
 import React from 'react';
+import axios from 'axios';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello World</h1>
-    </div>
-  );
+class App extends React.Component {
+
+  componentDidMount() {
+    axios.get(`https://jsonplaceholder.typicode.com/users`)
+      .then(res => {
+        const persons = res.data;
+        this.setState({ persons });
+    })
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Hello World</h1>
+      </div>
+    );
+  }
 }
 
 export default App;
