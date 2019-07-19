cf1c29a4b28d438c185b3a4a1374d52ca46d3032
HitzeD <d-hitze@comcast.net>
2019-07-19 19:14:02 -0400

    home page created

---
+++ mympy-app/src/view/home/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './home.scss';
+import Carousel from '../carousel/Carousel';
+
+const Home = () => {
+    return (
+        <div>
+            <Carousel />
+        </div>
+    )
+}
+
+export default Home;
