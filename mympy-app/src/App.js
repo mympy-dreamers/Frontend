90a99a847ad38367ed8195f42329a2b348d55afe
HitzeD <d-hitze@comcast.net>
2019-07-19 14:36:57 -0400

    added route and imported navbar

--- mympy-app/src/App.js
+++ mympy-app/src/App.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import axios from 'axios';
+import { Route } from 'react-router-dom';
+
+import NavBar from './view/navbar/NavBar';
 import './App.css';
 
 class App extends React.Component {
@@ -20,7 +23,9 @@ class App extends React.Component {
   render() {
     return (
       <div className="App">
+        <NavBar />
         <h1>{this.state.data.Title}</h1>
+        {/* not sure what actual paths will be, change once available */}
       </div>
     );
   }
