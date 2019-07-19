75d9fc94c397280dd7b8333a7d054e59f6df550d
HitzeD <d-hitze@comcast.net>
2019-07-19 19:14:22 -0400

    added router to index

--- mympy-app/src/index.js
+++ mympy-app/src/index.js
@@ -1,6 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, withRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const AppwithRouter = withRouter(App);
+ReactDOM.render(
+    <Router>
+        <AppwithRouter />
+    </Router>, 
+    document.getElementById('root')
+);
