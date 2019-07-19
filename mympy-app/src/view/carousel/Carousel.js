e7be0477f5bb2f979f46bd41134e991098ce9855
HitzeD <d-hitze@comcast.net>
2019-07-19 19:13:39 -0400

    carousel barbones laid down

---
+++ mympy-app/src/view/carousel/Carousel.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+import './carousel.scss';
+
+const Carousel = () => {
+    return (
+        <div className="caro-main">
+            <div className="left">
+                <div className="title">
+                    <h1 className="top-title">DREAM</h1>
+                    <h1 className="bottom-title">BIG</h1>
+                </div>
+                <div className="para-wrap">
+                    <p>Mympy Dreams is a marketplace where individuals 
+                        can create a profile and post their project and needs 
+                        and find funding from the Mymphy Dreams community.
+                    </p>
+                </div>
+            </div>
+            <div className="right"></div>
+        </div>
+    )
+}
+
+export default Carousel;
