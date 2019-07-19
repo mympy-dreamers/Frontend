d3b82eb05267c63669daafc002f6d377930d271f
HitzeD <d-hitze@comcast.net>
2019-07-19 14:37:26 -0400

    bare skeleton of navbar up learning sass to finish

---
+++ mympy-app/src/view/navbar/NavBar.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import '../../scss/navbar/navbar.scss';
+
+const NavBar = () => {
+    return (
+        <div className="main">
+            <div className="left">
+                <h1>Image goes here!</h1>
+            </div>
+            <div className="right">
+                <ul className="list">
+                    <li className="item">
+                    {/* Replace all 'href' with 'to' and all 'a' tags with 'Link' tags
+                        once routes are available
+                    */}
+                        <a href="#">Search For Dreamers</a>
+                    </li>
+                    <li className="item">
+                        <a href="#">Sign Up</a>
+                    </li>
+                    <li className="item">
+                        <a href="#">Login</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    )
+}
+
+export default NavBar;
