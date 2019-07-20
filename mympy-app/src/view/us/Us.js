b1fcb9113855406bbc3be617e9b17c5c28f7db69
HitzeD <d-hitze@comcast.net>
2019-07-19 20:25:25 -0400

    barebones for mid page

---
+++ mympy-app/src/view/us/Us.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+import './us.scss';
+
+const Us = () => {
+    return (
+        <div className="us-main">
+            <div className="left">
+                <div className="inner-left">
+                    <h1>What do we do</h1>
+                    <p>Many of wise words will be here!
+                    </p>
+                    <img src="" alt="" />
+                </div>
+            </div>
+            <div className="right">
+                <div className="inner-right">
+                    <h1>How do we do it</h1>
+                    <p>Many more wise words from our sponsors
+                    </p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Us;
