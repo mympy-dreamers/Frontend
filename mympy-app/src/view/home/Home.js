2ede71caf0aa56deefedc4120e24d4cc00e0c45f
HitzeD <d-hitze@comcast.net>
2019-07-19 20:25:38 -0400

    added mid page and bottom of page

--- mympy-app/src/view/home/Home.js
+++ mympy-app/src/view/home/Home.js
@@ -1,12 +1,16 @@
 import React from 'react';
 
 import './home.scss';
+import Us from '../us/Us';
+import Selector from '../selector/Selector';
 import Carousel from '../carousel/Carousel';
 
 const Home = () => {
     return (
         <div>
             <Carousel />
+            <Us />
+            <Selector />
         </div>
     )
 }
