3a6f7b3e38b621d6d656f08b40d5c4e3a02409b9
HitzeD <d-hitze@comcast.net>
2019-07-19 20:25:52 -0400

    bottom page bare bones in the works

---
+++ mympy-app/src/view/selector/Selector.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+import './selector.scss';
+
+const Selector = () => {
+    return (
+        <div className="select-main">
+            <div className="left">
+                <button>DREAMER</button>
+            </div>
+            <div className="offset">
+                <h1>ARE YOU A</h1>
+            </div>
+            <div className="right">
+                <button>SUPPORTER</button>
+            </div>
+        </div>
+    )
+}
+
+export default Selector;
