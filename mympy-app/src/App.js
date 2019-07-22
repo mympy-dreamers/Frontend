4ea5a0d61730b409b2ca97009b97338ac521be9c
HitzeD <d-hitze@comcast.net>
2019-07-22 18:06:04 -0400

    added in route for login

--- mympy-app/src/App.js
+++ mympy-app/src/App.js
@@ -4,7 +4,7 @@ import { Route } from 'react-router-dom';
 
 import NavBar from './view/navbar/NavBar';
 import Home from './view/home/Home';
-{/* import Login from './components/login/Login'; */}
+import Login from './components/login/Login';
 
 class App extends React.Component {
   state = {
@@ -20,7 +20,7 @@ class App extends React.Component {
       <div className="App">
         <NavBar />
         <Route exact path="/" component={Home} />
-        {/* <Route path="/login" component={Login} /> */}
+        <Route path="/login" component={Login} />
       </div>
     );
   }
