6bf14724cb6f65b55824e1562b600dea6792d520
HitzeD <d-hitze@comcast.net>
2019-07-22 18:08:36 -0400

    newUser barebones

---
+++ mympy-app/src/components/newUser/NewUser.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+
+class NewUser extends React.Component {
+    state = {
+
+    }
+
+    render(){
+        return(
+            <div className="newUse-main">
+
+            </div>
+        )
+    }
+}
+
+export default NewUser;
