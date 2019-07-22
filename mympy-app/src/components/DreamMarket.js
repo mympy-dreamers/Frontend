911600cc1075ea314cfda9d1e065e1d721183e1e
Krisli Dimo <krislidimo@gmail.com>
2019-07-22 17:18:05 -0500

    added card layout and linked with dummy data

--- mympy-app/src/components/DreamMarket.js
+++ mympy-app/src/components/DreamMarket.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+
+import '../scss/dreamMarket.scss';
 
 class DreamMarket extends React.Component {
   constructor() {
@@ -14,11 +17,32 @@ class DreamMarket extends React.Component {
     return (
       <div className="dream-market">
         <div className="cards-container">
-        	
+        	{this.props.dreams.map( dream => {
+
+        		//card
+        		return <div className="card" key={dream.id}>
+        			<Link to={`/DreamMarket/${dream.id}`}>
+
+        				<img className="card-img" src={dream.cardImg} />
+
+        				<div className="card-body">
+        					<div className="user-name">{dream.user.firstname + " " + dream.user.lastname}</div>
+        					<div className="dream-city">{dream.user.city}</div>
+        					<div className="dream-title">{dream.title}</div>
+        					<div className="description">{dream.shortDescription}</div>
+        					<button className="donate-button">Give $25</button>
+        				</div>
+        			</Link>
+        		</div>
+        	})}
         </div>
       </div>
     );
   }
 }
 
-export default DreamMarket;
+const mapStateToProps = ({ dreams }) => ({
+	dreams
+});
+
+export default connect(mapStateToProps)(DreamMarket);
