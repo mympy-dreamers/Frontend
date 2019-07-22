5cec9d73c12d1875dfbd83b8945c1a7596d74565
Krisli Dimo <krislidimo@gmail.com>
2019-07-22 16:34:10 -0500

    created DreamMarket component

---
+++ mympy-app/src/components/DreamMarket.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+class DreamMarket extends React.Component {
+  constructor() {
+  	super();
+
+  	this.state = {
+
+  	}
+  }
+
+  render() {
+    return (
+      <div className="dream-market">
+        <div className="cards-container">
+        	
+        </div>
+      </div>
+    );
+  }
+}
+
+export default DreamMarket;
