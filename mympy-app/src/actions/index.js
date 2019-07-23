cadaf3e1dde2d16695b23bb95085d045c4d5fe83
HitzeD <d-hitze@comcast.net>
2019-07-22 23:25:52 -0400

    updated endpoints to be live endpoints

--- mympy-app/src/actions/index.js
+++ mympy-app/src/actions/index.js
@@ -8,7 +8,7 @@ export const LOGIN_FAILURE = 'LOGIN_FAILURE';
 
 export const login = creds => dispatch => {
 	dispatch({ type: LOGIN_START });
-	axios.post('https://mympy-dreamers-staging.herokuapp.com/auth/login', creds)
+	axios.post('https://mympy-dreamers.herokuapp.com/auth/login', creds)
 	.then(res => {
 		localStorage.setItem('token', res.data.token);
 		dispatch({ type: LOGIN_SUCCESS, payload: res.data });
@@ -25,7 +25,7 @@ export const REGISTER_FAILURE = 'REGISTER_FAILURE';
 
 export const register = creds => dispatch => {
 	dispatch({ type: REGISTER_START });
-	axios.post('https://mympy-dreamers-staging.herokuapp.com/auth/register', creds)
+	axios.post('https://mympy-dreamers.herokuapp.com/auth/register', creds)
 	.then(res => {
 		console.log(res);
 		dispatch({ type: REGISTER_SUCCESS, payload: res.data })
