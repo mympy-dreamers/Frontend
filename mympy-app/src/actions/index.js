73f2872286b764d8b26d5ba2f3a7e48d5194853d
HitzeD <d-hitze@comcast.net>
2019-07-22 23:10:55 -0400

    login also complete and running

--- mympy-app/src/actions/index.js
+++ mympy-app/src/actions/index.js
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { bindActionCreators } from "redux";
 
 
 // LOGIN --------------------------------------------
@@ -9,10 +10,16 @@ export const LOGIN_FAILURE = 'LOGIN_FAILURE';
 export const login = creds => dispatch => {
 	dispatch({ type: LOGIN_START });
 	axios.post('https://mympy-dreamers-staging.herokuapp.com/auth/login', creds)
-	.then(res => console.log(res))
-	.catch(err => console.log(err));
+	.then(res => {
+		localStorage.setItem('token', res.data.token);
+		dispatch({ type: LOGIN_SUCCESS, payload: res.data });
+	})
+	.catch(err => {
+		dispatch({ type: LOGIN_FAILURE, payload: err });
+	});
 }
-// -------------------------------------------------
+// END OF LOGIN -------------------------------------------------
+// REGISTER -------------------------------------------------
 export const REGISTER_START = 'REGISTER_START';
 export const REGISTER_SUCCESS = 'REGISTER_SUCCESS';
 export const REGISTER_FAILURE = 'REGISTER_FAILURE';
@@ -22,7 +29,8 @@ export const register = creds => dispatch => {
 	axios.post('https://mympy-dreamers-staging.herokuapp.com/auth/register', creds)
 	.then(res => {
 		console.log(res);
-		dispatch({ type: REGISTER_SUCCESS, payload: res })
+		dispatch({ type: REGISTER_SUCCESS, payload: res.data })
 	})
 	.catch(err => console.log(err));
 }
+// END OF REGISTER -------------------------------------------------
