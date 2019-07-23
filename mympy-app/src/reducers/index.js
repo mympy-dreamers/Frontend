86374fe557fb03adb4db0bdbb005d60d8b65da61
HitzeD <d-hitze@comcast.net>
2019-07-22 23:10:38 -0400

    added items in state and reducers

--- mympy-app/src/reducers/index.js
+++ mympy-app/src/reducers/index.js
@@ -7,6 +7,8 @@ import { REGISTER_START, REGISTER_SUCCESS, REGISTER_FAILURE } from '../actions';
 
 const initialState = {
 	token: '',
+	message: '',
+	error: '',
 };
 
 const reducer = (state = initialState, action) => {
@@ -16,9 +18,14 @@ const reducer = (state = initialState, action) => {
 			};
 		case LOGIN_SUCCESS:
 			return {
+				...state,
+				token: action.payload.token,
+				message: action.payload.message,
 			};
 		case LOGIN_FAILURE:
 			return {
+				...state,
+				error: action.payload,
 			};
 		case REGISTER_START:
 			return {
@@ -28,12 +35,12 @@ const reducer = (state = initialState, action) => {
 		case REGISTER_SUCCESS:
 			return {
 				...state,
-				
+
 			}
 		case REGISTER_FAILURE:
 			return {
 				...state,
-
+				error: action.payload,
 			}
 		default:
 			return state;
