dd9efd4b56c67d1f8a1ea33d93d671ecaeece8a0
HitzeD <d-hitze@comcast.net>
2019-07-22 22:58:35 -0400

    added login and register routes, register working

--- mympy-app/src/actions/index.js
+++ mympy-app/src/actions/index.js
@@ -1,7 +1,28 @@
+import axios from "axios";
+
+
+// LOGIN --------------------------------------------
 export const LOGIN_START = 'LOGIN_START';
 export const LOGIN_SUCCESS = 'LOGIN_SUCCESS';
 export const LOGIN_FAILURE = 'LOGIN_FAILURE';
 
 export const login = creds => dispatch => {
 	dispatch({ type: LOGIN_START });
-};
+	axios.post('https://mympy-dreamers-staging.herokuapp.com/auth/login', creds)
+	.then(res => console.log(res))
+	.catch(err => console.log(err));
+}
+// -------------------------------------------------
+export const REGISTER_START = 'REGISTER_START';
+export const REGISTER_SUCCESS = 'REGISTER_SUCCESS';
+export const REGISTER_FAILURE = 'REGISTER_FAILURE';
+
+export const register = creds => dispatch => {
+	dispatch({ type: REGISTER_START });
+	axios.post('https://mympy-dreamers-staging.herokuapp.com/auth/register', creds)
+	.then(res => {
+		console.log(res);
+		dispatch({ type: REGISTER_SUCCESS, payload: res })
+	})
+	.catch(err => console.log(err));
+}
