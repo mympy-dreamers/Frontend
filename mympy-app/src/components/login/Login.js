f9113143d608c35fade4d339d64c3df82a774ec5
HitzeD <d-hitze@comcast.net>
2019-07-23 16:11:31 -0400

    added reactstrap import

--- mympy-app/src/components/login/Login.js
+++ mympy-app/src/components/login/Login.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { login, register } from '../../actions/index';
+import React from 'react';
+import { Spinner } from 'reactstrap';
 
 class Login extends React.Component {
     constructor(props){
@@ -39,7 +41,7 @@ class Login extends React.Component {
                 ...this.state.signup,
                 [e.target.name]: e.target.value,
             },
-            match: true,
+            match: null,
         })
     }
 
@@ -54,50 +56,46 @@ class Login extends React.Component {
         })
     }
 
+    passwordMatch = () => {
+
+    }
+
     register = e => {
         e.preventDefault();
         this.props.register(this.state.signup);
-        this.setState({
-            signup: {
-                email: '',
-                username: '',
-                password: '',
-            },
-            confirm: '',
-        });
-        this.props.history.push('/login');
+        this.props.history.push('/login')
     }
 
     render(){
         return (
             <div className="login-main">
-            {(this.props.type === 'login') ? (
-                <div className="login">
-                    <h3>LOG IN</h3>
-                    <form onSubmit={this.login}>
-                        <h4>Username</h4>
-                        <input onChange={this.handleLoginChanges} name="username" type="text" placeholder="Enter Username" value={this.state.cred.username} />
-                        <h4>Password</h4>
-                        <input onChange={this.handleLoginChanges} name="password" type="password" placeholder="Enter Password" value={this.state.cred.password} />
-                        <button>Next</button>
-                    </form>
-                </div>
-            ) : (
-                <div className="newUse-main">
-                    <h3>SIGN UP</h3>
-                    <form onSubmit={this.register}>
-                        <h4>Email</h4>
-                        <input onChange={this.handleSignupChanges} placeholder="Enter Email" name="email" value={this.state.signup.email}/>
-                        <h4>Username</h4>
-                        <input onChange={this.handleSignupChanges} placeholder="Enter Username" name="username" value={this.state.signup.username}/>
-                        <h4>Password</h4>
-                        <input onChange={this.handleSignupChanges} placeholder="Enter Password" type="password" name="password" value={this.state.signup.password}/>
-                        <h4>Confirm Password</h4>
-                        <input onChange={this.handleConfirmChange} placeholder="Confirm Your Password" type="password" name="confirm" value={this.state.confirm}/>
-                        <button>Submit</button>
-                    </form>
-                </div>
-            )}
+                {(this.props.type === 'login') ? (
+                    <div className="login">
+                        <h3>LOG IN</h3>
+                        <form onSubmit={this.login}>
+                            <h4>Username</h4>
+                            <input onChange={this.handleLoginChanges} name="username" type="text" placeholder="Enter Username" value={this.state.cred.username} />
+                            <h4>Password</h4>
+                            <input onChange={this.handleLoginChanges} name="password" type="password" placeholder="Enter Password" value={this.state.cred.password} />
+                            <button>Next</button>
+                        </form>
+                    </div>
+                ) : (
+                    <div className="newUse-main">
+                        <h3>SIGN UP</h3>
+                        <form onSubmit={this.register}>
+                            <h4>Email</h4>
+                            <input onChange={this.handleSignupChanges} placeholder="Enter Email" name="email" value={this.state.signup.email}/>
+                            <h4>Username</h4>
+                            <input onChange={this.handleSignupChanges} placeholder="Enter Username" name="username" value={this.state.signup.username}/>
+                            <h4>Password</h4>
+                            <input onChange={this.handleSignupChanges} placeholder="Enter Password" type="password" name="password" value={this.state.signup.password}/>
+                            <h4>Confirm Password</h4>
+                            <input onChange={this.handleConfirmChange} placeholder="Confirm Your Password" type="password" name="confirm" value={this.state.confirm}/>
+                            <button>Submit</button>
+                        </form>
+                    </div>
+                )}
             </div>
         )
     }
