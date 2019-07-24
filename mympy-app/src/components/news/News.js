c5d2dcb8ca0f63a57fa4273632a52747f6c83682
HitzeD <d-hitze@comcast.net>
2019-07-24 17:46:13 -0400

    News Component wire

---
+++ mympy-app/src/components/news/News.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+class News extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+
+        }
+    }
+
+    render() {
+        return(
+            <div className="news-main">
+                <h3>I am News!</h3>
+            </div>
+        )
+    }
+}
+
+export default News;
