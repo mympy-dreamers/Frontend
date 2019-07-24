254fc019430d826cc5179637a613795aa67d1303
HitzeD <d-hitze@comcast.net>
2019-07-24 17:46:21 -0400

    Stats component wire

---
+++ mympy-app/src/components/stats/Stats.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+class Stats extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+
+        }
+    }
+
+    render() {
+        return(
+            <div className="stats-main">
+                <h3>Stats Here!</h3>
+            </div>
+        )
+    }
+}
+
+export default Stats;
