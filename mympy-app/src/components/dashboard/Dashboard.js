03f9791e8e468463d9db3886e9d52c383ba94cf3
HitzeD <d-hitze@comcast.net>
2019-07-24 17:03:55 -0400

    initial commit

---
+++ mympy-app/src/components/dashboard/Dashboard.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+class Dashboard extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+
+        }
+    }
+
+    render() {
+        return(
+            <div className="dash-main">
+                <h1>I am the Dashboard!</h1>
+            </div>
+        )
+    }
+}
+
+export default Dashboard;
