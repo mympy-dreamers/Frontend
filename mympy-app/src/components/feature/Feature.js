137ea2a48d5581dcebfc2dec8feb00f99ffd5ae4
HitzeD <d-hitze@comcast.net>
2019-07-24 17:46:03 -0400

    Feature component wire

---
+++ mympy-app/src/components/feature/Feature.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+class Feature extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+
+        }
+    }
+
+    render() {
+        return(
+            <div>
+                <h1>I am Feature</h1>
+            </div>
+        )
+    }
+}
+
+export default Feature;
