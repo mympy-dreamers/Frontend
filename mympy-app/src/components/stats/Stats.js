62ccb7d58bd1427c67807c81b769a626338fbc4f
HitzeD <d-hitze@comcast.net>
2019-07-25 17:27:58 -0400

    added barebones stats component

--- mympy-app/src/components/stats/Stats.js
+++ mympy-app/src/components/stats/Stats.js
@@ -11,7 +11,16 @@ class Stats extends React.Component {
     render() {
         return(
             <div className="stats-main">
-                <h3>Stats Here!</h3>
+                <div className="myDreams">
+                    <h1>My Dreams</h1>
+                    <h5>Progress Here</h5>
+                    <button>more info</button>
+                </div>
+                <div className="support-dreams">
+                    <h1>Dreams Supported</h1>
+                    <h5>Progress Here</h5>
+                    <button>more info</button>
+                </div>
             </div>
         )
     }
