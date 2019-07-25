964e4710224dfb3ca63cdc05d8398382124f5474
HitzeD <d-hitze@comcast.net>
2019-07-25 17:28:22 -0400

    struggling with alignment

--- mympy-app/src/components/news/News.js
+++ mympy-app/src/components/news/News.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import { connect } from 'react-redux';
+
+
 
 class News extends React.Component {
     constructor() {
@@ -11,10 +14,21 @@ class News extends React.Component {
     render() {
         return(
             <div className="news-main">
-                <h3>I am News!</h3>
+                <div className="news-wrap">
+                    <div className="img-wrap">
+                        <img src={this.props.news.image} alt="" />
+                    </div>
+                    <div className="text-wrap">
+                        <p>{this.props.news.text}</p>
+                    </div>
+                </div>
             </div>
         )
     }
 }
 
-export default News;
+const mapStateToProps = state => ({
+    news: state.news,
+})
+
+export default connect(mapStateToProps, {})(News);
