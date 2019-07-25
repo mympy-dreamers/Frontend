1200222471b2d87e09738507790553cd02fd414f
HitzeD <d-hitze@comcast.net>
2019-07-25 11:18:17 -0400

    added feature component insides

--- mympy-app/src/components/feature/Feature.js
+++ mympy-app/src/components/feature/Feature.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import { connect } from 'react-redux';
+
+import demo from '../../img/demo.jpg'
 
 class Feature extends React.Component {
     constructor() {
@@ -8,13 +11,48 @@ class Feature extends React.Component {
         }
     }
 
+    componentDidMount(){
+        
+    }
+
     render() {
         return(
-            <div>
-                <h1>I am Feature</h1>
+            <div className="feature-main">
+                <div className="wrapper">
+
+                    <h3>Feature Dreamers</h3>
+
+                    <div className="featured">
+                        {/* Component will go here of featured -- DUMMY DATA CURRENTLY */}
+                        {this.props.featured.map(f => {
+                            return(
+                                <div className="single" key={f.id} style={{backgroundImage: demo}}>
+                                    <div className="info-wrap">
+                                        <div className="name-city">
+                                            <h3>{f.user.username}</h3>
+                                            <h4>{f.user.city}</h4>
+                                        </div>
+                                        <div className="type">
+                                            <h3>{f.type}</h3>
+                                        </div>
+                                        <p>{f.shortDescription}</p>
+                                        <div className="progressBar">
+                                            <h1>Progress bar here</h1>
+                                        </div>
+                                        <button>Learn More</button>
+                                    </div>
+                                </div>
+                            )
+                        })}
+                    </div>
+                </div>
             </div>
         )
     }
 }
 
-export default Feature;
+const mapStateToProps = state => ({
+    featured: state.featured,
+})
+
+export default connect(mapStateToProps, {})(Feature);
