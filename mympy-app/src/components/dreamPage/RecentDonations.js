5c2bf8b0ccad7fe4fa2ec77eef3fab48de428843
Krisli Dimo <krislidimo@gmail.com>
2019-07-28 23:11:45 -0500

    finished RecentDonations component

---
+++ mympy-app/src/components/dreamPage/RecentDonations.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+import './recentDonations.scss';
+
+const RecentDonations = (props) => {
+	return (
+		<div className="recent-donations">
+			<h2 className="header">Recent Donations</h2>
+  		
+  		<div className="donations-container">
+  			{console.log(props.donations)}
+				{props.donations.map( donation => {
+
+					return <div className="donation" key={donation.donation}>
+						<img className="user-img"/>
+						<div className="donations-name-wrapper">
+							<h2 className="amount">{"$" + donation.donation}</h2>
+							<p className="name">{donation.user.firstname + " " + donation.user.lastname}</p>
+						</div>
+					</div>
+				})}
+  		</div>
+   	</div>
+	)
+}
+
+export default RecentDonations;
