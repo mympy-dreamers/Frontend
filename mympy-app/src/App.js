0b7eab8c98d088082bdb50e1bcb31ae984d560b7
Krisli Dimo <krislidimo@gmail.com>
2019-07-28 23:04:27 -0500

    added route for DreamPage to /market/:id

--- mympy-app/src/App.js
+++ mympy-app/src/App.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
+import Login from './components/login/Login';
 import NavBar from './view/navbar/NavBar';
 import Home from './view/home/Home';
 import DreamMarket from './components/dreamMarket/DreamMarket';
-import Login from './components/login/Login';
+import DreamPage from './components/dreamPage/DreamPage.js';
 
 class App extends React.Component {
   state = {
     
   }
-
 
   render() {
     return (
@@ -31,6 +31,7 @@ class App extends React.Component {
             />
           )} />
           <Route exact path="/market" component={DreamMarket} />
+          <Route path="/market/:id" component={DreamPage} />
         </div>
       </Router>
     );
