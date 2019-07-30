aa95d275d97a656f25a9020ab9ef1ac3aca70f11
HitzeD <d-hitze@comcast.net>
2019-07-30 16:13:58 -0400

    finally showing the username in the top bar

--- mympy-app/src/components/userBar/UserBar.js
+++ mympy-app/src/components/userBar/UserBar.js
@@ -1,18 +1,54 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { Spinner } from 'reactstrap';
 
+import { fetchAllDreams } from '../../actions';
+
+
+
+// used to decode the token for the username
+
+const token = localStorage.getItem('token');
+
+// End of token decoding
 class UserBar extends React.Component {
-    constructor() {
+    constructor(){
         super();
         this.state = {
-
+            username: '',
         }
+        
+    }
+
+    componentDidMount(){
+        this.props.fetchAllDreams();
+    }
+
+    isReady = () => {
+        const name = localStorage.getItem('username');
+        if(name === null){
+            return(
+                <Spinner size="sm" color="secondary" />
+            )
+        } else {
+            return (
+                <h3>Hello {name.toUpperCase()}!</h3>
+            )
+        }
+    }
+
+    // method for decoding a token
+
+    decodeToken = token => {
+        const decodedToken = JSON.parse(atob(token.split('.')[1]));
+        return decodedToken;
     }
 
     render() {
         return(
             <div className="user-main">
                 <div className="wrapper">
-                    <h3>Hello User!</h3>
+                    {this.isReady()}
                     <div className="flexing">
                         <p>img here</p>
                         <p>create dream</p>
@@ -27,4 +63,4 @@ class UserBar extends React.Component {
     }
 }
 
-export default UserBar;
+export default connect(null, { fetchAllDreams })(UserBar);
