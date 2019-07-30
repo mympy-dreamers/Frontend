de3d8323665a1000b631d4de8ff9cba03eb28595
HitzeD <d-hitze@comcast.net>
2019-07-30 19:47:51 -0400

    comment format

--- mympy-app/src/actions/index.js
+++ mympy-app/src/actions/index.js
@@ -52,6 +52,7 @@ export const register = creds => dispatch => {
 // END OF REGISTER -------------------------------------------------
 
 // FETCH DREAMS ----------------------------------------------------
+
 export const FETCH_DREAMS_START = 'FETCH_DREAMS_START';
 export const FETCH_DREAMS_SUCCESS = 'FETCH_DREAMS_SUCCESS';
 export const FETCH_DREAMS_FAILURE = 'FETCH_DREAMS_FAILURE';
@@ -64,20 +65,27 @@ export const fetchAllDreams = () => dispatch => {
 	})
 	.catch(err => console.log(err));
 }
+
 // END OF FETCH DREAMS ---------------------------------------------
 
 // FETCH USER BY ID ------------------------------------------------
+
 export const FETCH_USER_BY_ID_START = 'FETCH_USER_BY_ID_START';
 export const FETCH_USER_BY_ID_SUCCESS = 'FETCH_USER_BY_ID_SUCCESS';
 export const FETCH_USER_BY_ID_FAILURE = 'FETCH_USER_BY_ID_FAILURE';
 
 export const fetchUserById = (id) => dispatch => {
 	dispatch({ type: FETCH_USER_BY_ID_START });
-	axios.get(`http://mympy-dreamers.herokuapp.com/api/${id}`, token)
+	axios.get(`http://mympy-dreamers.herokuapp.com/api/user/${id}`, token)
 	.then(res => {
 		console.log(res);
 		dispatch({ type: FETCH_USER_BY_ID_SUCCESS, payload: res.data });
 	})
 	.catch(err => console.log(err));                                                   
 }
+
 // END OF FETCH USER BY ID ------------------------------------------
+
+//     ----------------------------------------------------
+
+//  END OF   ----------------------------------------------------
