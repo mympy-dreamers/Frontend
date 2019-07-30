b2226ea8426adc37af04a6be5541396ed6625258
HitzeD <d-hitze@comcast.net>
2019-07-30 19:47:10 -0400

    merge conflict resolved

--- mympy-app/src/App.js
+++ mympy-app/src/App.js
@@ -1,18 +1,19 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
+import Login from './components/login/Login';
 import NavBar from './view/navbar/NavBar';
 import Home from './view/home/Home';
 import DreamMarket from './components/dreamMarket/DreamMarket';
 import Login from './components/login/Login';
 import Dashboard from './components/dashboard/Dashboard';
 import PrivateRoute from './components/login/PrivateRoute';
+import DreamPage from './components/dreamPage/DreamPage.js';
 
 class App extends React.Component {
   state = {
     
   }
-
 
   render() {
     return (
@@ -33,6 +34,7 @@ class App extends React.Component {
             />
           )} />
           <Route exact path="/market" component={DreamMarket} />
+          <Route path="/market/:id" component={DreamPage} />
           <PrivateRoute exact path="/dashboard" component={Dashboard} />
         </div>
       </Router>
---
+++ mympy-app/src/components/dreamPage/TopSection.js
@@ -0,0 +1,99 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const TopSectionDiv = styled.div`
+  display: flex;
+  justify-content: center;
+  margin-top: 2rem;
+  margin-bottom: 8rem;
+	height: 500px;
+  border: 1px solid black;
+
+  .banner {
+		width: 70%;
+  }
+
+  .right-dash {
+  	display: flex;
+		flex-direction: column;
+		align-items: center;
+		width: 30%;
+		padding: 32px;
+
+		.data-viz {
+			border: 7px solid #92CC62;
+			border-radius: 50%;
+			width: 150px;
+			height: 150px;
+			margin-bottom: 16px;
+		}
+
+		.days-left {
+			font-size: 22px;
+			font-weight: bold;	
+			margin-bottom: 20px;
+		}
+
+		.give-btn {
+			width: 160px;
+			height: 66px;
+			border-radius: 33px;
+			background-color: #0A2243;
+			color: white;
+			font-size: 26px;
+			font-weight: bold;
+			border: none;
+			margin-bottom: 24px;
+		}
+
+		.share-title {
+			font-size: 18px;
+			font-weight: bold;
+			margin-bottom: 8px;
+		}
+
+		.share-buttons {
+			display: flex;
+			justify-content: space-between;
+			width: 116px;
+			margin-bottom: 16px;
+		}
+		.contact-section {
+			display: flex;
+			width: 119px;
+			justify-content: space-between;
+			align-items: center;
+			
+			h4 {
+				font-size: 18px;
+				font-weight: bold;
+			}
+		}
+  }
+`;
+
+const TopSection = () => {
+	return (
+		<TopSectionDiv>
+			<img className="banner" src="https://images.unsplash.com/photo-1506031765313-0bc574a405f0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1374&q=80" />
+  		
+  		<div className="right-dash">
+				<img className="data-viz" />
+				<h3 className="days-left">7 Days Left</h3>
+				<button className="give-btn"><img />Give</button>
+				<h4 className="share-title">Share Dream</h4>
+				<div className="share-buttons">
+					<i class="fab fa-facebook fa-5x"></i>
+					<i class="fab fa-instagram fa-5x"></i>
+				</div>
+				<div className="contact-section">
+					<h4>CONTACT</h4>
+					<i class="fas fa-envelope fa-2x"></i>
+				</div>
+
+  		</div>
+   	</TopSectionDiv>
+	)
+}
+
+export default TopSection;
