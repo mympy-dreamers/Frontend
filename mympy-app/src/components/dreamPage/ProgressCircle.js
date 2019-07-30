471e91afdaa1da81fd00eef890fc13d95402da45
Krisli Dimo <krislidimo@gmail.com>
2019-07-30 00:56:18 -0500

    commented code

--- mympy-app/src/components/dreamPage/ProgressCircle.js
+++ mympy-app/src/components/dreamPage/ProgressCircle.js
@@ -2,14 +2,16 @@ import React, { Component } from "react";
 import { VictoryPie, VictoryLabel, VictoryAnimation } from "victory";
 
 export default class ProgressCircle extends React.Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
     this.state = {
       percent: 0, data: this.getData(0)
     };
   }
 
   componentDidMount() {
+    //if component mounts set state to given percent at an interval(constantly updating)
+    //change percent to donationsRecived/donationGoal when back-end is seeded
     let percent = 75;
     this.setStateInterval = window.setInterval(() => {
       // percent += (Math.random() * 25);
@@ -22,6 +24,7 @@ export default class ProgressCircle extends React.Component {
   }
 
   componentWillUnmount() {
+    //if component unmounts clear interval
     window.clearInterval(this.setStateInterval);
   }
 
@@ -44,6 +47,7 @@ export default class ProgressCircle extends React.Component {
             labels={() => null}
             style={{
               data: { fill: (d) => {
+                /*Sets color of path based on percentage it's current at*/
                 const color = d.y > 30 ? "#92CC62" : "red";
                 return d.x === 1 ? color : "lightgrey";
               }}
@@ -52,6 +56,7 @@ export default class ProgressCircle extends React.Component {
           <VictoryAnimation duration={1000} data={this.state}>
             {(newProps) => {
               return (
+                /*Percentage inside the circle*/
                 <VictoryLabel
                   textAnchor="middle" verticalAnchor="middle"
                   x={200} y={200}
