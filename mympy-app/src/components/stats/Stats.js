1598281df8bd2158ebac73b32ef0d22afbe82f23
HitzeD <d-hitze@comcast.net>
2019-07-30 19:16:11 -0400

    used proper className

--- mympy-app/src/components/stats/Stats.js
+++ mympy-app/src/components/stats/Stats.js
@@ -13,12 +13,16 @@ class Stats extends React.Component {
             <div className="stats-main">
                 <div className="myDreams">
                     <h1>My Dreams</h1>
-                    <h5>Progress Here</h5>
+                    <div className="progress">
+                        <div className="progress-bar progress-bar-striped bg-success" role="progressbar" style={{width: "37%"}} aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
+                    </div>
                     <button>more info</button>
                 </div>
                 <div className="support-dreams">
                     <h1>Dreams Supported</h1>
-                    <h5>Progress Here</h5>
+                    <div className="progress">
+                        <div className="progress-bar progress-bar-striped bg-success" role="progressbar" style={{width: "79%"}} aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
+                    </div>
                     <button>more info</button>
                 </div>
             </div>
