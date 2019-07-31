e7803b0ef361cd6f4310bc5da7f1faca04142493
Manju KC <Manju KC>
2019-07-31 15:01:54 -0400

    updated dreamsearch component

--- mympy-app/src/components/search_bar/DreamSerachbar.js
+++ mympy-app/src/components/search_bar/DreamSerachbar.js
@@ -1,10 +1,30 @@
 import React from "react";
+import { connect } from 'react-redux';
+import { updateSearch } from '../../actions'
 
-const SearchBar= (props) => {
-return(
-    <form>
-    <input className="input" type="text" placeholder="Search" onKeyUp={props.search}/>
-</form> 
-)
+
+class SearchBar extends React.Component{
+    state = {
+        search: ''
+    }
+
+     changeSearch = (e) => {
+        let value = e.target.value
+        this.setState(state => {
+            return {search: value}
+        }, () => {
+            this.props.updateSearch(this.state.search)
+        })
+        
+    }
+    render(){
+        return(
+            <form>
+            <input className="input" type="text" value={this.state.search} placeholder="Search" onChange={this.changeSearch}/>
+        </form> 
+        )
+    }
 }
- export default SearchBar;
+
+
+ export default connect(null, {updateSearch})(SearchBar)
