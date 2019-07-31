301d63fae45e58bd752ea143602ab76a366db144
tashi woeser <tashiwoeser@tashis-MacBook-Pro.local>
2019-07-31 15:41:54 -0700

    state changes made on dreamCard

--- mympy-app/src/components/dreamForms/dreamCard.js
+++ mympy-app/src/components/dreamForms/dreamCard.js
@@ -7,23 +7,44 @@ class DreamCard extends React.Component {
     super();
   
     this.state = {
-        // dreamCard: [],
-        dreamCards: {
+        dreamCard: [{}],
         fullName: '',
         city: '',
         aboutDream: '',
+        inputText: ''
      }
-    }
 }
 
 handleChanges = e => {
-    console.log(this.handleChanges)
-const {fullName , value} = e.target
-this.setState({ [fullName]: value})
+    console.log(e.target.value)
+    this.setState({
+    [e.target.name]: e.target.value
+    })
 }
+
+addInfo = e => {
+    e.preventDefault()
+    this.setState({
+        dreamCard: [...this.state.dreamCard, {dreamCard: this.state.inputText, 
+            inputText: '',
+            fullName: '',
+            city: '',
+            aboutDream: ''
+        }]
+    })
+}
+
+// onSubmitHandler = e => {
+//     const {dreamerPage, value} = e.target.fullName
+//     this.setState({[dreamerPage]: value})
+// }
   
-    render() {
-    return (
+render() {
+return (
+    <div className='dream-Home-Page'> 
+        <div>
+              <img className='dream-Home-Img' src='http://localhost:3000/static/media/galaxyvr2x.0af499b3.png'></img>
+        </div>
         <div className='dreamer-card-app'>
 
           <div className='form-title'>
@@ -32,22 +53,23 @@ this.setState({ [fullName]: value})
 
           <form className='dreamer-card'>
             <div className='inputs'>
-
-            <div>
-              <input className='input-style' placeholder='Full Name' value={this.state.fullName} onChange={this.handleChanges}></input>
-            </div>
-            <div>
-              <input className='input-style' placeholder='City' value={this.state.city} onchange={this.handleChanges} ></input>
-            </div>
-            <div>
-              <input id='dream-box' className='input-style' placeholder='Dream' value={this.state.aboutDream}></input>   
-            </div>
-
+                <div>
+                <input className='input-style' placeholder='Full Name' value={this.state.fullName} onChange={this.handleChanges}></input>
+                </div>
+                <div>
+              <input className='input-style' placeholder='City' value={this.state.city} onChange={this.handleChanges}></input>
+                </div>
+                <div>
+                <input id='dream-box' className='input-style' placeholder='Dream' value={this.state.aboutDream} onChange={this.handleChanges}></input>   
+                </div>
             </div> {/* inputs end */}
-            <button className='submit-button'>Submit</button>
+
+            <button onClick={this.addInfo} type='submit' className='submit-button'>Submit</button>
+
           </form> {/* dreamer-card end  */}
 
-        </div>  /* dreamer-card-app end */
+        </div>  {/* dreamer-card-app end  */}
+    </div> /* dream-Home-Page end */
 )
 }
 }
---
+++ mympy-app/src/components/dreamForms/dreamerCard.js
@@ -0,0 +1,15 @@
+ import React from 'react';
+ import dreamCard from '../dreamForms/dreamCard.js'
+
+ class DreamerProfile extends React.Component {
+  
+ render() {
+     return (
+         <div>
+
+         </div>
+     )
+ }
+ }
+
+ export default DreamerProfile;
