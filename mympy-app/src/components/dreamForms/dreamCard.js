23c7a1c851b1399d44a65ebb70344f2e5676266b
tashi woeser <tashiwoeser@tashis-MacBook-Pro.local>
2019-07-31 13:56:07 -0700

    setting state and handlechange

--- mympy-app/src/components/dreamForms/dreamCard.js
+++ mympy-app/src/components/dreamForms/dreamCard.js
@@ -7,33 +7,47 @@ class DreamCard extends React.Component {
     super();
   
     this.state = {
-     postCard : {
-        firstName: '',
-        lastName: '',
+        // dreamCard: [],
+        dreamCards: {
+        fullName: '',
         city: '',
         aboutDream: '',
-        email: ''
      }
+    }
 }
+
+handleChanges = e => {
+    console.log(this.handleChanges)
+const {fullName , value} = e.target
+this.setState({ [fullName]: value})
 }
+  
     render() {
     return (
-        <div className='dreamer-card'>
+        <div className='dreamer-card-app'>
 
           <div className='form-title'>
               <h1>Create a Dream</h1>
           </div>
 
-          <form>
-          <div className='inputs'>
-              <input className='input-style' placeholder='First name'></input>
-              <input className='input-style' placeholder='Last name'></input>
-              <input className='input-style' placeholder='City'></input>
-              <input className='input-style' placeholder='Dream'></input>
-              <input className='input-style' placeholder='email'></input>
-          </div>
-          </form>
-        </div>  /* dreamer-card end */
+          <form className='dreamer-card'>
+            <div className='inputs'>
+
+            <div>
+              <input className='input-style' placeholder='Full Name' value={this.state.fullName} onChange={this.handleChanges}></input>
+            </div>
+            <div>
+              <input className='input-style' placeholder='City' value={this.state.city} onchange={this.handleChanges} ></input>
+            </div>
+            <div>
+              <input id='dream-box' className='input-style' placeholder='Dream' value={this.state.aboutDream}></input>   
+            </div>
+
+            </div> {/* inputs end */}
+            <button className='submit-button'>Submit</button>
+          </form> {/* dreamer-card end  */}
+
+        </div>  /* dreamer-card-app end */
 )
 }
 }
