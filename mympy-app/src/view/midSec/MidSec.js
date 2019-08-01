79f94b86b58aedc67967b5d20d06294714ddd408
HitzeD <d-hitze@comcast.net>
2019-08-01 19:42:22 -0400

    added in mid section and styling

--- mympy-app/src/view/midSec/MidSec.js
+++ mympy-app/src/view/midSec/MidSec.js
@@ -1,9 +1,29 @@
 import React from 'react';
 
+import midSwirl from '../../img/midSwirl.png';
+import meteor from '../../img/meteor.png';
+import female from '../../img/female.svg';
+
 const MidSec = () => {
     return (
         <div className="mid-main">
-            
+            <div className="top-mid">
+                <div className="div-wrap">
+                    <div className="top-div">
+                        <h1>Are you a Dreamer or Supporter?</h1>
+                    </div>
+                    <div className="btns">
+                        <button className="dreamer">Dreamer</button>
+                        <button className="supporter">Supporter</button>
+                    </div>
+                    <div className="meteor">
+                        <img className="meteor-img" src={meteor} alt="meteor" />
+                    </div>
+                </div>
+            </div>
+            <div className="empty">
+            <img src={female} alt="female" />
+            </div>
         </div>
     )
 }
