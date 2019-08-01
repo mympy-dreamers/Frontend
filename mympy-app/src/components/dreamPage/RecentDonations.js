5a036a30317bfc9e03eedddde5bf2f63be55c2b0
HitzeD <d-hitze@comcast.net>
2019-08-01 11:49:56 -0400

    added demo img src

--- mympy-app/src/components/dreamPage/RecentDonations.js
+++ mympy-app/src/components/dreamPage/RecentDonations.js
@@ -1,8 +1,10 @@
 import React from 'react';
 
 import './recentDonations.scss';
+import demo from '../../img/demo.jpg';
 
 const RecentDonations = (props) => {
+	console.log(props.donations);
 	return (
 		<div className="recent-donations">
 			<h2 className="header">Recent Donations</h2>
@@ -10,7 +12,7 @@ const RecentDonations = (props) => {
   		<div className="donations-container">
 				{props.donations.map( donation => {
 					return <div className="donation" key={donation.donation}>
-						<img className="user-img" alt="" />
+						<img className="user-img" src={demo} alt="" />
 						<div className="donations-name-wrapper">
 							<h2 className="amount">{"$" + donation.donation}</h2>
 							<p className="name">{donation.user.firstname + " " + donation.user.lastname}</p>
