93e40ea9903d423630f2ecf816d7fda2871b4d79
HitzeD <d-hitze@comcast.net>
2019-08-02 10:04:24 -0400

    base dreamer component

---
+++ mympy-app/src/view/dreamer/Dreamer.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+import rocket from '../../img/rocket.svg';
+
+const Dreamer = () => {
+    return (
+        <div className="dream-main">
+            <img className="rocket" src={rocket} alt="rocket" />
+            <div className="top">
+                <h1>Be a Dreamer</h1>
+                <p>Poster can be one of the effective marketing and advertising materials. 
+                    It is also a great tool to use when you want to present your services to a 
+                    new league or as a point of sale display on picture day. The poster must show 
+                    your products that are available, along with your company name and contact information. 
+                    If you having trouble about creating a good 4 color poster print, a 4 color printing 
+                    company that has 4 color poster printing service may be able to help you made your poster.
+                </p>
+            </div>
+        </div>
+    )
+}
+
+export default Dreamer;
