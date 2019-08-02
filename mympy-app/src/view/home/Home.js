f8cc5afd64a46bf14b1db62d0e3eb3510a8863c7
HitzeD <d-hitze@comcast.net>
2019-08-02 13:23:42 -0400

    added img parallax effect

--- mympy-app/src/view/home/Home.js
+++ mympy-app/src/view/home/Home.js
@@ -8,23 +8,40 @@ import Dreamer from '../dreamer/Dreamer';
 
 import swirl from '../../img/swirl.png';
 import bPoint from '../../img/break.png';
+import rocket from '../../img/rocket.svg';
+
+const breakStyle = {
+    width: "100%",
+    height: "450px",
+    backgroundImage: `url(${bPoint})`,
+    backgroundAttachment: "fixed",
+    backgroundPosition: "center",
+    backgroundRepeat: "no-repeat",
+    backgroundSize: "cover",
+}
+
+const bottomStyle = {
+    width: "100%",
+    height: "450px",
+    backgroundImage: `url(${swirl})`,
+}
 
 const Home = () => {
     return (
         <div className="home-main">
             <Carousel />
-            <div className="break">
-                <img src={bPoint} alt="scene" />
+            <div className="break" style={ breakStyle }>
             </div>
-            <Us />
-            <MidSec />
-            <Support />
-            <div className="break">
-                <img src={bPoint} alt="scene" />
-            </div>
-            <Dreamer />
-            <div>
-                <img src={swirl} alt="styling" />
+            <div className="rocket-contain">
+                <img className="rocket" src={rocket} alt="" />
+                <Us />
+                <MidSec />
+                <Support />
+                <div className="break" style={ breakStyle }>
+                </div>
+                <Dreamer />
+                <div className="break" style={ bottomStyle }>
+                </div>
             </div>
         </div>
     )
