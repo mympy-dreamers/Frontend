054ba3d12ef68f5e3334bc0735303dc8cb16a560
ParrishJ <jarparrish@gmail.com>
2019-08-02 13:07:29 -0700

    initialized app FE testing

--- mympy-app/src/App.test.js
+++ mympy-app/src/App.test.js
@@ -1,9 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { render, fireEvent } from '@testing-library/react';
+import '@testing-library/react/cleanup-after-each';
+
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+describe('<App />', () => {
+
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<App />, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
+
+  it('renders the app component with producing any errors', () => {
+    const renderApp = render(<App />);
+  })
+
+})
+
+
+
+
