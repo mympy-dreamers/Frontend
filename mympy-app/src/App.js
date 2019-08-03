4c6064c8fd5277e6e15c0e89203919786ea4cfb3
Nickolas Stricker <43568886+NickStrick@users.noreply.github.com>
2019-08-02 19:51:40 -0500

    added env stuff

--- mympy-app/src/App.js
+++ mympy-app/src/App.js
@@ -9,32 +9,36 @@ import DreamPage from './components/dreamPage/DreamPage.js';
 import Dashboard from './components/dashboard/Dashboard';
 import PrivateRoute from './components/login/PrivateRoute';
 
+import dotenv from 'dotenv'
+dotenv.config();
+
 class App extends React.Component {
   state = {
-    
+
   }
 
   render() {
+    console.log(process.env.DOMAIN)
     return (
-        <div className="App">
-          <NavBar />
-          <Route exact path="/" component={Home} />
-          <Route path="/login" render={(props) => (
-            <Login
+      <div className="App">
+        <NavBar />
+        <Route exact path="/" component={Home} />
+        <Route path="/login" render={(props) => (
+          <Login
             {...props}
             type="login"
-            />
-          )} />
-          <Route path="/register" render={(props) => (
-            <Login
+          />
+        )} />
+        <Route path="/register" render={(props) => (
+          <Login
             {...props}
             type="register"
-            />
-          )} />
-          <Route exact path="/market" component={DreamMarket} />
-          <Route path="/market/:id" component={DreamPage} />
-          <PrivateRoute exact path="/dashboard" component={Dashboard} />
-        </div>
+          />
+        )} />
+        <Route exact path="/market" component={DreamMarket} />
+        <Route path="/market/:id" component={DreamPage} />
+        <PrivateRoute exact path="/dashboard" component={Dashboard} />
+      </div>
     );
   }
 }
