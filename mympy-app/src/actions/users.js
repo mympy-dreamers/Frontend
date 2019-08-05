8a96d638656c4aed76f6acdc5324eebdc6cb781e
Mylynh Nguyen <34496865+nmylynh@users.noreply.github.com>
2019-08-04 18:42:58 -0700

    Update users.js

--- mympy-app/src/actions/users.js
+++ mympy-app/src/actions/users.js
@@ -1,15 +1,123 @@
 import axios from 'axios';
 
-export const FETCH_USER_BY_ID_START = 'FETCH_USER_BY_ID_START';
-export const FETCH_USER_BY_ID_SUCCESS = 'FETCH_USER_BY_ID_SUCCESS';
-export const FETCH_USER_BY_ID_FAILURE = 'FETCH_USER_BY_ID_FAILURE';
+export const FETCH_USERS_START = 'FETCH_USERS_START';
+export const FETCH_USERS_SUCCESS = 'FETCH_USERS_SUCCESS';
+export const FETCH_USERS_FAILURE = 'FETCH_USERS_FAILURE';
 
-export const fetchUserById = (id) => dispatch => {
-	dispatch({ type: FETCH_USER_BY_ID_START });
-	axios.get(`http://mympy-dreamers.herokuapp.com/api/user/${id}`, token)
-	.then(res => {
-		console.log(res);
-		dispatch({ type: FETCH_USER_BY_ID_SUCCESS, payload: res.data });
-	})
-	.catch(err => console.log(err));                                                   
+export const FETCH_USER_START = "FETCH_USER_START"
+export const FETCH_USER_SUCCESS = "FETCH_USER_SUCCESS"
+export const FETCH_USER_FAILURE = "FETCH_USER_FAILURE"
+
+export const FETCH_USER_DREAMS_START = 'FETCH_USER_DREAMS_START';
+export const FETCH_USER_DREAMS_SUCCESS = 'FETCH_USER_DREAMS_SUCCESS';
+export const FETCH_USER_DREAMS_FAILURE = 'FETCH_USER_DREAMS_FAILURE';
+
+export const UPDATE_USER_START = 'UPDATE_USER_START';
+export const UPDATE_USER_SUCCESS = 'UPDATE_USER_SUCCESS';
+export const UPDATE_USER_FAILURE = 'UPDATE_USER_FAILURE';
+
+export const DELETE_USER_START = 'DELETE_USER_START';
+export const DELETE_USER_SUCCESS = 'DELETE_USER_SUCCESS';
+export const DELETE_USER_FAILURE = 'DELETE_USER_FAILURE';
+
+
+const BASE_URL = 'https://mympy-dreamers.herokuapp.com';
+
+const token = {
+    headers: { authorization: localStorage.getItem('jwt') }
+}
+
+export const fetchUsers = () => (dispatch) => {
+    dispatch({ type: FETCH_USERS_START });
+
+    axios
+    .get(`${BASE_URL}/api/users`, token)
+    .then(res => {
+        dispatch({
+            type: FETCH_USERS_SUCCESS,
+            payload: res.data
+        })
+    })
+    .catch(err => {
+        dispatch({
+            type: FETCH_USERS_FAILURE,
+            payload: err  
+        })
+    })
+}
+
+export const fetchUserById = (id) => (dispatch) => {
+    dispatch({ type: FETCH_USER_START })
+
+    axios
+    .get(`${BASE_URL}/api/users/${id}`, token)
+    .then(res => {
+        dispatch({
+            type: FETCH_USER_SUCCESS,
+            payload: res.data
+        })
+    })
+    .catch(err => {
+        dispatch({
+            type: FETCH_USER_FAILURE,
+            payload: err
+        })
+    })
+}
+
+export const fetchUserDreams = (id) => (dispatch) => {
+    dispatch({ type: FETCH_USER_DREAMS_START })
+
+    axios
+    .get(`${BASE_URL}/api/users/${id}/dreams`, token)
+    .then(res => {
+        dispatch({
+            type: FETCH_USER_DREAMS_SUCCESS,
+            payload: res.data
+        })
+    })
+    .catch(err => {
+        dispatch({
+            type: FETCH_USER_DREAMS_FAILURE,
+            payload: err
+        })
+    })
+}
+
+export const updateUser = (id, changes) => dispatch => {
+    dispatch({ type: UPDATE_USER_START});
+
+    axios
+    .put(`${BASE_URL}/api/users/${id}`, changes, token)
+    .then(res => {
+        dispatch({
+            type: UPDATE_USER_SUCCESS,
+            payload: res.data
+        })
+    })
+    .catch(err => {
+        dispatch({
+            type: UPDATE_USER_FAILURE,
+            payload: err
+        })
+    })
+}
+
+export const deleteUser = (id) => dispatch => {
+    dispatch({ type: DELETE_USER_START });
+
+    axios
+    .delete(`${BASE_URL}/api/users/${id}`, token)
+    .then(res => {
+        dispatch({
+            type: DELETE_USER_SUCCESS,
+            payload: id
+        })
+    })
+    .catch(err => {
+        dispatch({
+            type: DELETE_USER_FAILURE,
+            payload: err
+        })
+    })
 }
