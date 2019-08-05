88975897f2cd03dbfe21eac5c9d6aa3ea2dba801
Mylynh Nguyen <34496865+nmylynh@users.noreply.github.com>
2019-08-04 18:42:51 -0700

    Update auth.js

--- mympy-app/src/actions/auth.js
+++ mympy-app/src/actions/auth.js
@@ -1,39 +1,43 @@
 import axios from 'axios';
 import jwtDecode from 'jwt-decode';
 
-const BASE_URL = 'https://mympy-dreamers.herokuapp.com';
+export const REGISTER_START = 'REGISTER_START';
+export const REGISTER_SUCCESS = 'REGISTER_SUCCESS';
+export const REGISTER_FAILURE = 'REGISTER_FAILURE';
 
-export const REGISTER_START = "REGISTER_START"
-export const REGISTER_SUCCESS = "REGISTER_SUCCESS"
-export const REGISTER_FAILURE = "REGISTER_FAILURE"
+export const LOGIN_START = 'LOGIN_START';
+export const LOGIN_SUCCESS = 'LOGIN_SUCCESS';
+export const LOGIN_FAILURE = 'LOGIN_FAILURE';
+
+export const LOGOUT_USER = 'LOGOUT_USER'
+
+
+const BASE_URL = 'https://mympy-dreamers.herokuapp.com';
 
 export const register = (user) => dispatch => {
     dispatch({ type: REGISTER_START });
 
-    axios.post(`${BASE_URL}/auth/register`, user)
-        .then(res => {
-            localStorage.setItem('jwt', res.data.token);
-            dispatch({
-                type: REGISTER_SUCCESS,
-                payload: res.data
-            })
+    axios
+    .post(`${BASE_URL}/auth/register`, user)
+    .then(res => {
+        localStorage.setItem('jwt', res.data.token);
+        dispatch({
+            type: REGISTER_SUCCESS,
+            payload: res.data
         })
-        .catch(err => {
-            dispatch({
-                type: REGISTER_FAILURE,
-                payload: err
-            })
-        console.log(err)
+    })
+    .catch(err => {
+        dispatch({
+            type: REGISTER_FAILURE,
+            payload: err
         })
+    })
 }
 
-export const LOGIN_START = "LOGIN_START"
-export const LOGIN_SUCCESS = "LOGIN_SUCCESS"
-export const LOGIN_FAILURE = "LOGIN_FAILURE"
-
 export const login = (creds) => dispatch => {
-dispatch({ type: LOGIN_START });
-axios
+    dispatch({ type: LOGIN_START });
+
+    axios
     .post(`${BASE_URL}/auth/login`, creds)
     .then(res => {
         localStorage.setItem('jwt', res.data.token);
@@ -41,7 +45,6 @@ axios
             type: LOGIN_SUCCESS,
             payload: jwtDecode(res.data.token)
         })
-        
     })
     .catch(err => { 
         dispatch({
@@ -50,8 +53,6 @@ axios
         })
     })
 }
-
-export const LOGOUT_USER = "LOGOUT_USER"
 
 export const logout = () => {
     return {
