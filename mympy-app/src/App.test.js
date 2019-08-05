d1f9f83b3224c2f07521c2108693f82aa936539f
ParrishJ <jarparrish@gmail.com>
2019-08-05 16:09:40 -0700

    more added to testing

--- mympy-app/src/App.test.js
+++ mympy-app/src/App.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { render, fireEvent } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import '@testing-library/react/cleanup-after-each';
 
 import App from './App';
