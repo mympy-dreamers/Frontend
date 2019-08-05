9c0e7155389daabcb8edbd478e4c046bc4073b83
Mylynh Nguyen <34496865+nmylynh@users.noreply.github.com>
2019-08-04 18:43:09 -0700

    Create users.js

---
+++ mympy-app/src/reducers/users.js
@@ -0,0 +1,58 @@
+import {
+    FETCH_USERS_START, FETCH_USERS_SUCCESS, FETCH_USERS_FAILURE,
+    DELETE_USER_START, DELETE_USER_SUCCESS, DELETE_USER_FAILURE
+} from '../actions'
+
+
+const INITIAL_STATE = {
+    fetching: false,
+    deletingUser: false,
+    errors: [],
+    users: [] 
+}
+
+
+export default function usersReducer(state=INITIAL_STATE, action) {
+    switch(action.type) {
+        case FETCH_USERS_START:
+            return {
+                ...state,
+                fetching: true
+            }
+        case FETCH_USERS_SUCCESS:
+            return {
+                ...state, 
+                fetching: false,
+                users: action.payload
+            }
+        case FETCH_USERS_FAILURE:
+            return {
+                ...state,
+                fetching: false,
+                errors: action.payload
+            }
+        case DELETE_USER_START:
+            return {
+                ...state,
+                deletingUser: true
+            }
+        case DELETE_USER_SUCCESS:
+            const { users } = state;
+            const filteredUsers = users.filter(user => {
+                return user.id !== action.payload;
+            });
+            return {
+                ...state,
+                deletingUser: false,
+                users: [ ...filteredUsers ]
+            }
+        case DELETE_USER_FAILURE:
+            return {
+                ...state,
+                deletingUser: false,
+                errors: action.payload
+            }
+        default:
+            return state
+    }
+}
