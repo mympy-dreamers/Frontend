00d4e0a8c83d52bdb33b832a79209f857dad015a
Mylynh Nguyen <34496865+nmylynh@users.noreply.github.com>
2019-08-05 00:03:53 -0700

    Update DreamMarket.js

--- mympy-app/src/components/dreamMarket/DreamMarket.js
+++ mympy-app/src/components/dreamMarket/DreamMarket.js
@@ -45,8 +45,10 @@ class DreamMarket extends React.Component {
   }
 }
 
-const mapStateToProps = ({ dreams }) => ({
-	dreams
-});
+const mapStateToProps = (state) => {
+    return {
+        dreams: state.dreams.featured
+    }
+}
 
 export default connect(mapStateToProps)(DreamMarket);
