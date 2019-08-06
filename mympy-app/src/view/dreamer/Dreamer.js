0fc26e80ed8c7ca4ed1db298c95cc1db2c761f39
HitzeD <d-hitze@comcast.net>
2019-08-06 15:57:49 -0400

    added dreamer image

--- mympy-app/src/view/dreamer/Dreamer.js
+++ mympy-app/src/view/dreamer/Dreamer.js
@@ -1,13 +1,18 @@
 import React from 'react';
 
-import rocket from '../../img/rocket.svg';
 import otherSwirl from '../../img/otherSwirl.svg';
+
+const bottomStyle = {
+    backgroundImage: `url(${otherSwirl})`,
+}
 
 const Dreamer = () => {
     return (
         <div className="dream-main">
             <div className="top">
-                <h1>Be a Dreamer</h1>
+                <div>
+                    <h1>Be a Dreamer</h1>
+                </div>
                 <p>Poster can be one of the effective marketing and advertising materials. 
                     It is also a great tool to use when you want to present your services to a 
                     new league or as a point of sale display on picture day. The poster must show 
@@ -16,8 +21,8 @@ const Dreamer = () => {
                     company that has 4 color poster printing service may be able to help you made your poster.
                 </p>
             </div>
-            <div className="bottom">
-                
+            <div className="bottom" style={{ bottomStyle }}>
+                <img className="cx" src="#" alt="customer" />
             </div>
         </div>
     )
