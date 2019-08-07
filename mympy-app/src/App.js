4a38a817c8cc5a6f70350a3a46dd0b3dd9fa5131
Manju KC <Manju KC>
2019-08-07 16:15:36 -0400

    created AboutUs file

--- mympy-app/src/App.js
+++ mympy-app/src/App.js
@@ -8,6 +8,7 @@ import DreamMarket from './components/dreamMarket/DreamMarket';
 import DreamPage from './components/dreamPage/DreamPage.js';
 import Dashboard from './components/dashboard/Dashboard';
 import PrivateRoute from './components/login/PrivateRoute';
+import About from './view/aboutPage/AboutUs';
 
 class App extends React.Component {
   state = {
@@ -31,6 +32,7 @@ class App extends React.Component {
             type="register"
             />
           )} />
+          <Route exact path="/about" component = {About}/>
           <Route exact path="/market" component={DreamMarket} />
           <Route path="/market/:id" component={DreamPage} />
           <PrivateRoute exact path="/dashboard" component={Dashboard} />
---
+++ mympy-app/src/view/aboutPage/AboutUs.js
@@ -0,0 +1,18 @@
+
+import React from "react";
+import group from '../../img/woman.svg'
+
+const AboutUs = () => {
+    return (
+        <div>
+            <div>
+            <h1>ABOUT US</h1>
+            <p>Mympy Dreams is a marketplace where individuals can create a profile and post
+             their project and needs and find funding from the Mymphy Dreams community.</p>
+             <img src={group}/>
+             </div>
+        </div>
+    )
+}
+
+export default AboutUs;
