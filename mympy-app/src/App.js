df90418c5260753c6e3010ee0afeb831ef18ea85
Nickolas Stricker <43568886+NickStrick@users.noreply.github.com>
2019-08-07 16:29:22 -0500

    refactored app routes

--- mympy-app/src/App.js
+++ mympy-app/src/App.js
@@ -8,26 +8,15 @@ import DreamMarket from './components/dreamMarket/DreamMarket';
 import DreamPage from './components/dreamPage/DreamPage.js';
 import Dashboard from './components/dashboard/Dashboard';
 import PrivateRoute from './components/login/PrivateRoute';
-import { useAuth0 } from "./react-auth0-wrapper";
 
 class App extends React.Component {
-  state = {
-
-  }
-
-  componentDidMount() {
-    // const { loading, user } = useAuth0();
-    // if (localStorage.getItem('user') !== user.name) {
-    //   this.history.push('/dashboard')
-    // }
-  }
 
   render() {
-    // localStorage.setItem('user', useAuth0().user.name)
+
     return (
       <div className="App">
         <NavBar />
-        <Route exact path="/" component={Home} />
+        <Route exact path="/" render={(props) => <Home {...props} />} />
         <Route path="/login" render={(props) => (
           <Login
             {...props}
