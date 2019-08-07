5b8fa940f13fcb8b5303cb4ce095d103600057e1
Manju KC <Manju KC>
2019-08-07 19:18:16 -0400

    added paragraph and img in AboutUs component

--- mympy-app/src/view/aboutPage/AboutUs.js
+++ mympy-app/src/view/aboutPage/AboutUs.js
@@ -1,18 +1,31 @@
-
 import React from "react";
-import group from '../../img/woman.svg'
+import group from "../../img/woman.svg";
+import "./About.css";
 
 const AboutUs = () => {
-    return (
-        <div>
-            <div>
-            <h1>ABOUT US</h1>
-            <p>Mympy Dreams is a marketplace where individuals can create a profile and post
-             their project and needs and find funding from the Mymphy Dreams community.</p>
-             <img src={group}/>
-             </div>
+  return (
+    <div>
+      <div className="maindiv">
+        <div className="first">
+          <div className="leftside">
+            <h1>ABOUT <span>US</span></h1>
+            <p>
+            Mympy Dreams is a marketplace where individuals can create a
+              profile and post their project and needs and find funding from the
+              Mymphy Dreams community.profile and post their project and needs and 
+              find funding from the
+              Mymphy Dreams community.
+            </p>
+          </div>
+          <div className="rightside">
+            <img src={group} />
+          </div>
         </div>
-    )
-}
+      </div>
+
+    
+    </div>
+  );
+};
 
 export default AboutUs;
