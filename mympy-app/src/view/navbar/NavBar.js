de5194b5b65381b119ca30cf94944af10acbb0d7
HitzeD <d-hitze@comcast.net>
2019-08-07 15:55:07 -0400

    added wrap div back into code for style

--- mympy-app/src/view/navbar/NavBar.js
+++ mympy-app/src/view/navbar/NavBar.js
@@ -8,30 +8,32 @@ import logo from '../../img/MIMPYlogo.svg';
 const NavBar = (props) => {
     return (
         <div className="main">
-            <div className="left">
-                <Link to="/">
-                    <img src={logo} alt="Company Logo" />
-                </Link>
-            </div>
-            <div>
-                { props.show && <Searchbar/> }    
+            <div className="nav-wrap">
+                <div className="left">
+                    <Link to="/">
+                        <img src={logo} alt="Company Logo" />
+                    </Link>
                 </div> 
-            <div className="right"> 
-                <ul className="list">
-                    <li className="item">
-                        <Link to="/">Home</Link>
-                    </li>
-                    <li className="item">
-                        <Link to="/market">Search For Dreams</Link>
-                    </li>
-                   
-                    <li className="item">
-                        <Link to="/register">Sign Up</Link>
-                    </li>
-                    <li className="item">
-                        <Link to="/login">Sign In</Link>
-                    </li>
-                </ul>
+                <div>
+                    { props.show && <Searchbar/> }    
+                </div>
+                <div className="right">                 
+                    <ul className="list">
+                        <li className="item">
+                            <Link to="/">Home</Link>
+                        </li>
+                        <li className="item">
+                            <Link to="/market">Search For Dreams</Link>
+                        </li>
+                    
+                        <li className="item">
+                            <Link to="/register">Sign Up</Link>
+                        </li>
+                        <li className="item">
+                            <Link to="/login">Sign In</Link>
+                        </li>
+                    </ul>
+                </div>
             </div>
         </div>
     )
