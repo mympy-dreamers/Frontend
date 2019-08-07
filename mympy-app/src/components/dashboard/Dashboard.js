ead71952db692ea4271a4d760789c7d3ab97e0c4
Nickolas Stricker <43568886+NickStrick@users.noreply.github.com>
2019-08-07 16:32:50 -0500

    fixed routing to dashboard to work with auth0

--- mympy-app/src/components/dashboard/Dashboard.js
+++ mympy-app/src/components/dashboard/Dashboard.js
@@ -5,30 +5,23 @@ import Stats from '../stats/Stats';
 import News from '../news/News';
 import Featured from '../featured/Featured';
 
-class Dashboard extends React.Component {
-    constructor() {
-        super();
-        this.state = {
+const Dashboard = () => {
 
-        }
-    }
-
-    render() {
-        return(
-            <div className="dash-main">
-                <div className="top-bar">
-                    <UserBar />
-                </div>
-                <div className="mid-sec">
-                    <Stats />
-                    <News />
-                </div>
-                <div className="feature">
-                    <Featured />
-                </div>
+    return (
+        <div className="dash-main">
+            <div className="top-bar">
+                <UserBar />
             </div>
-        )
-    }
+            <div className="mid-sec">
+                <Stats />
+                <News />
+            </div>
+            <div className="feature">
+                <Featured />
+            </div>
+        </div>
+    )
+
 }
 
 export default Dashboard;
