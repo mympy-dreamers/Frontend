072ca0b81b13aef93272328db65ac56a2a21e3fd
Manju KC <Manju KC>
2019-08-07 23:06:02 -0400

    added team descrition and changed some style

--- mympy-app/src/view/aboutPage/AboutUs.js
+++ mympy-app/src/view/aboutPage/AboutUs.js
@@ -1,6 +1,6 @@
 import React from "react";
 import group from "../../img/woman.svg";
-import rainbowball from "../../img/rainbowball.svg"
+import rainbowball from "../../img/rainbowball.svg";
 
 import "./About.css";
 
@@ -36,13 +36,29 @@ const AboutUs = () => {
               Mymphy Dreams community.profile and post their project and needs
               and find funding from the Mymphy Dreams community.
             </p>
-          </div> 
+          </div>
         </div>
         <div>
-        <img  className="img2" src={rainbowball}/>
+          <img className="img2" src={rainbowball} />
+        </div>
       </div>
-      </div>
-      <div>
+      <div />
+
+      <div className="team1">
+        <div className="team2">
+          <h1 className="teamheader">The Mympy Team</h1>
+          <p className="teamparagraph">
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy text
+            ever since the 1500s, when an unknown printer took a galley of type
+            and scrambled it to make a type specimen book. It has survived not
+            only five centuries, but also the leap into electronic typesetting,
+            remaining essentially unchanged. It was popularised in the 1960s
+            with the release of Letraset sheets containing Lorem Ipsum passages,
+            and more recently with desktop publishing software like Aldus
+            PageMaker including versions of Lorem Ipsum.
+          </p>
+        </div>
       </div>
     </div>
   );
