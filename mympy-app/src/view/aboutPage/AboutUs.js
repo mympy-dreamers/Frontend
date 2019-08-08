b64520a06857c0b72cbcbd3f40dde494e15c5c02
Manju KC <Manju KC>
2019-08-07 21:55:22 -0400

    added mission and changed style

--- mympy-app/src/view/aboutPage/AboutUs.js
+++ mympy-app/src/view/aboutPage/AboutUs.js
@@ -1,5 +1,6 @@
 import React from "react";
 import group from "../../img/woman.svg";
+
 import "./About.css";
 
 const AboutUs = () => {
@@ -8,13 +9,14 @@ const AboutUs = () => {
       <div className="maindiv">
         <div className="first">
           <div className="leftside">
-            <h1>ABOUT <span>US</span></h1>
+            <h1 className="header0ne">
+              ABOUT <span>US</span>
+            </h1>
             <p>
-            Mympy Dreams is a marketplace where individuals can create a
+              Mympy Dreams is a marketplace where individuals can create a
               profile and post their project and needs and find funding from the
-              Mymphy Dreams community.profile and post their project and needs and 
-              find funding from the
-              Mymphy Dreams community.
+              Mymphy Dreams community.profile and post their project and needs
+              and find funding from the Mymphy Dreams community.
             </p>
           </div>
           <div className="rightside">
@@ -23,7 +25,22 @@ const AboutUs = () => {
         </div>
       </div>
 
-    
+      <div className="about3">
+        <div className="about2">
+          <div className="about1">
+            <h1 className="header">Our Mission</h1>
+            <p className="mission">
+              Mympy Dreams is a marketplace where individuals can create a
+              profile and post their project and needs and find funding from the
+              Mymphy Dreams community.profile and post their project and needs
+              and find funding from the Mymphy Dreams community.
+            </p>
+          </div>
+        </div>
+      </div>
+
+      <div>
+      </div>
     </div>
   );
 };
