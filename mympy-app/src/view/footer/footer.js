ef4f4272f04b9dde26f5306e1c37d2121cb7d38e
Nickolas Stricker <43568886+NickStrick@users.noreply.github.com>
2019-08-14 16:54:18 -0500

    fixed footer errors

--- mympy-app/src/view/footer/footer.js
+++ mympy-app/src/view/footer/footer.js
@@ -7,34 +7,34 @@ class Footer extends React.Component {
         return (
 
             <div>
-            <div className='menu-footer'>
+                <div className='menu-footer'>
 
-                <div className='menu-icons'>
-                    <div id='fb-icon'>
-                    <a href='#'><i class="fab fa-facebook-f"></i></a>
+                    <div className='menu-icons'>
+                        <div id='fb-icon'>
+                            <a href='#'><i className="fab fa-facebook-f"></i></a>
+                        </div>
+                        <div id='ig-icon'>
+                            <a href='#'><i className="fab fa-instagram"></i></a>
+                        </div>
+                        <div id='yelp-icon'>
+                            <a href='#'><i className="fab fa-yelp"></i></a>
+                        </div>
+                    </div> {/* menu-icons end */}
+
+                    <div className='links'>
+                        <Link id='home' to='/dashboard'>Home</Link>
+                        <Link id='about' to='/about'>About</Link>
                     </div>
-                    <div id='ig-icon'>
-                    <a href='#'><i class="fab fa-instagram"></i></a>
+
+                    <div className='footer-home-link'>
+                        <h4><span>Myphy Dream</span> <span>124 Conch Street, Bikini Bottom 95476</span> <span>mymphydream@gmail.com</span></h4>
+                    </div> {/* footer-home-link end */}
+
+                    <div className='copyright'>
+                        <p>Copyright © 2019, Myphy Dream. All Rights Reserved.</p>
                     </div>
-                    <div id='yelp-icon'>
-                    <a href='#'><i class="fab fa-yelp"></i></a>
-                    </div>
-                </div> {/* menu-icons end */}
+                </div> {/* menu-footer end */}
 
-                 <div className='links'>
-                    <Link id='home' to='/dashboard'>Home</Link>
-                    <Link id='about' to='/about'>About</Link>
-                </div>
-
-                <div className='footer-home-link'>
-                    <h4><span>Myphy Dream</span> <span>124 Conch Street, Bikini Bottom 95476</span> <span>mymphydream@gmail.com</span></h4>
-                </div> {/* footer-home-link end */}
-
-                <div className='copyright'>
-                    <p>Copyright © 2019, Myphy Dream. All Rights Reserved.</p>
-                </div>
-            </div> {/* menu-footer end */}
-                
             </div>
 
         )
