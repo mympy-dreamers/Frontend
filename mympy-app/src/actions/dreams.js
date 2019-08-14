577734c56e138326c65e4649b4fc0611396cd1f3
Nickolas Stricker <43568886+NickStrick@users.noreply.github.com>
2019-08-14 16:59:07 -0500

    fixed action baseUrl assignment

--- mympy-app/src/actions/dreams.js
+++ mympy-app/src/actions/dreams.js
@@ -28,8 +28,7 @@ const token = {
     headers: { authorization: localStorage.getItem('jwt') }
 }
 
-// const BASE_URL = process.env.REACT_APP_BE_URL || 'http://localhost:5000';
-const BASE_URL = 'https://mympy-dreamers-staging.herokuapp.com';
+const BASE_URL = process.env.REACT_APP_BE_URL || 'http://localhost:5000';
 
 export const fetchDreamById = (id) => (dispatch) => {
     dispatch({ type: FETCH_DREAM_START });
--- mympy-app/src/actions/sendGrid.js
+++ mympy-app/src/actions/sendGrid.js
@@ -4,13 +4,12 @@ export const SEND_EMAIL_START = 'SEND_EMAIL_START';
 export const SEND_EMAIL_SUCCESS = 'SEND_EMAIL_SUCCESS';
 export const SEND_EMAIL_FAILURE = 'SEND_EMAIL_FAILURE';
 
-// const BASE_URL = process.env.REACT_APP_BE_URL || 'http://localhost:5000';
-const BASE_URL = 'https://mympy-dreamers-staging.herokuapp.com';
+const BASE_URL = process.env.REACT_APP_BE_URL || 'http://localhost:5000';
 
 export const sendMail = (mailing) => dispatch => {
     dispatch({ type: SEND_EMAIL_START });
 
     axios.post(`${BASE_URL}/mail/`, mailing)
-    .then(res => console.log(res))
-    .catch(err => console.log(err));
+        .then(res => console.log(res))
+        .catch(err => console.log(err));
 }
