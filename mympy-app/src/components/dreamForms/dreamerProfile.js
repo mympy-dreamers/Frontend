af1aef0830585353b5b27e4342f5c34ab3dec5e8
HitzeD <d-hitze@comcast.net>
2019-08-14 11:16:50 -0400

    merge changes

--- mympy-app/src/components/dreamForms/dreamerProfile.js
+++ mympy-app/src/components/dreamForms/dreamerProfile.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import {connect} from "react-redux";
-import {addDream} from '../../actions';
+import { connect } from "react-redux";
+import { addDream } from '../../actions';
 import { Button, Form, FormGroup, Label, Input, FormText } from 'reactstrap';
 
 import './dreamCard.css';
@@ -15,31 +15,42 @@ class DreamInfo extends React.Component {
                 dream_long_description: '',
                 donations_received: 1,
                 donation_goal: 0,
-                user_id: 1,
+                user_id: null,
             }
         };
     }
 
+    componentDidMount() {
+        if (this.props.user) {
+            this.setState({
+                dreamCard: { ...this.state.dreamCard, user_id: this.props.user.id }
+            })
+        } else {
+            console.log('cannot find user id for forms page')
+        }
+    }
+
     handleChanges = e => {
         e.preventDefault()
-        this.setState ({ 
-            ...this.state, // does this cause problems? there isn't anything to spread in
+        this.setState({
+            ...this.state,
             dreamCard: {
-                ...this.state.dreamCard, 
+                ...this.state.dreamCard,
                 [e.target.name]: e.target.value
             }
         })
     }
 
     handleSubmit = e => {
-        const newDream = {...this.state.dreamCard, donation_goal: parseInt(this.state.dreamCard.donation_goal, 10)}
+        const newDream = { ...this.state.dreamCard, donation_goal: parseInt(this.state.dreamCard.donation_goal, 10) }
         e.preventDefault()
-        console.log(newDream);
+        console.log('submittedDream: ', newDream);
         this.props.addDream(newDream)
             .then(() => this.props.history.push('/addDream/image'));
     }
 
     render() {
+        console.log(this.state)
         return (
             <div className='dream-Home-Page'>
                 <div className='dreamer-card-app'>
@@ -47,28 +58,34 @@ class DreamInfo extends React.Component {
                         <h1>Make your dream in reality!</h1>
                     </div>
                     <div>
-                    <FormGroup>
-                        <Label  className="dreamlable" for="exampleEmail">Dream Name</Label>
-                        <Input className="dreamform" onChange={this.handleChanges} name="dream_name" id="dream_name" placeholder="Enter your dream here" />
-                    </FormGroup>
-                    <FormGroup>
-                        <Label for="examplePassword">Donation Goals</Label>
-                        <Input onChange={this.handleChanges}  name="donation_goal" id="donation_goal" placeholder="Enter Donation goal here" />
-                    </FormGroup>
-                    <FormGroup>
-                        <Label for="exampleText">Give us a short descrption</Label>
-                        <Input onChange={this.handleChanges} type="textarea" name="dream_short_description" id="dream_short_description" />
-                    </FormGroup>
-                    <FormGroup>
-                        <Label for="exampleText">Give us a long descrption</Label>
-                        <Input onChange={this.handleChanges} type="textarea" name="dream_long_description" id="dream_long_description" />
-                    </FormGroup>
-                    <Button onClick={this.handleSubmit}>Submit</Button>
-                    </div> 
+                        <FormGroup>
+                            <Label className="dreamlable" for="exampleEmail">Dream Name</Label>
+                            <Input className="dreamform" onChange={this.handleChanges} name="dream_name" id="dream_name" placeholder="Enter your dream here" />
+                        </FormGroup>
+                        <FormGroup>
+                            <Label for="examplePassword">Donation Goals</Label>
+                            <Input onChange={this.handleChanges} name="donation_goal" id="donation_goal" placeholder="Enter Donation goal here" />
+                        </FormGroup>
+                        <FormGroup>
+                            <Label for="exampleText">Give us a short descrption</Label>
+                            <Input onChange={this.handleChanges} type="textarea" name="dream_short_description" id="dream_short_description" />
+                        </FormGroup>
+                        <FormGroup>
+                            <Label for="exampleText">Give us a long descrption</Label>
+                            <Input onChange={this.handleChanges} type="textarea" name="dream_long_description" id="dream_long_description" />
+                        </FormGroup>
+                        <Button onClick={this.handleSubmit}>Submit</Button>
+                    </div>
                 </div>  {/* dreamer-card-app end  */}
             </div> /* dream-Home-Page end */
         )
     }
 }
+const mapStateToProps = ({ users, auth }) => {
+    return {
+        authZeroUser: users.authZeroUser,
+        user: auth.user
+    }
+}
 
-export default connect(null, { addDream })(DreamInfo);
+export default connect(mapStateToProps, { addDream })(DreamInfo);
