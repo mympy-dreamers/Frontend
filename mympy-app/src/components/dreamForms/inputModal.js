f8088fa83ac9899ad7cf8eda7c3395d7d41356d3
Nickolas Stricker <43568886+NickStrick@users.noreply.github.com>
2019-08-15 17:44:18 -0500

    added cancel button and changed onclicks

--- mympy-app/src/components/dreamForms/inputModal.js
+++ mympy-app/src/components/dreamForms/inputModal.js
@@ -1,20 +1,21 @@
 import React from 'react';
-import {Modal, ModalHeader, ModalBody, ModalFooter, Button } from 'reactstrap';
+import { Modal, ModalHeader, ModalBody, ModalFooter, Button } from 'reactstrap';
 
+import './dreamCard.css';
 
 const bodyStyle = {
   fontSize: "25px",
-  height:"120px",
-  marginTop:"40px",
-  
+  height: "120px",
+  marginTop: "40px",
+
 
 }
 
 const headStyle = {
-  color:"red",
+  color: "red",
   fontSize: "22px",
   color: '#1B5485',
- 
+
 }
 
 
@@ -25,12 +26,16 @@ class InputModal extends React.Component {
     return (
       <div>
         <Modal isOpen={this.props.showModal}>
-        <ModalHeader style={ headStyle }>Warning!!</ModalHeader>
-          <ModalBody style={ bodyStyle}>
-           "Are you sure you want to save your dream?"
+          <ModalHeader style={headStyle}>Warning!!</ModalHeader>
+          <ModalBody style={bodyStyle}>
+            "Are you sure you want to save your dream?"
           </ModalBody>
           <ModalFooter>
-              <Button onClick={this.props.closeModal}>Okey</Button>
+            <Button id='cancel-button' onClick={() => this.props.closeModal()}>Cancel</Button>
+            <Button onClick={() => {
+              this.props.closeModal()
+              this.props.handleFinalSubmit()
+            }}>Submit Dream</Button>
           </ModalFooter>
         </Modal>
       </div>
