83524644fa068bcc290d2a7293fcb179890af3e8
HitzeD <d-hitze@comcast.net>
2019-08-15 18:06:20 -0400

    percent loads,illhas nan at first then right value

--- mympy-app/src/components/stats/Stats.js
+++ mympy-app/src/components/stats/Stats.js
@@ -1,24 +1,60 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
-import ProgressCircle from '../dreamPage/ProgressCircle';
+import UserProgressCircle from '../dreamPage/UserProgressCircle';
 
 class Stats extends React.Component {
     constructor() {
         super();
         this.state = {
-
+            dreamGoal: 0,
+            dreamCurr: 0,
+            
         }
     }
 
+    componentDidUpdate(prevProps) {
+        if (this.props.user.dreams !== prevProps.user.dreams) {
+            this.props.user.dreams.map(dream => {       
+                return (
+                    this.setState({
+                        dreamGoal: this.state.dreamGoal + dream.donation_goal,
+                        dreamCurr: this.state.dreamCurr + dream.donations_received,
+                    })
+                )
+            })
+          }
+    }
+
+    // componentDidMount(){
+    //     setTimeout(() => {
+    //         this.getDreamData();
+    //     }, 900);
+    // }
+
+    getDreamData = () => {
+        this.props.user.dreams && this.props.user.dreams.map(dream => {       
+            return (
+                this.setState({
+                    dreamGoal: this.state.dreamGoal + dream.donation_goal,
+                    dreamCurr: this.state.dreamCurr + dream.donations_received,
+                })
+            )
+        })
+    }
+
+
     render() {
+        console.log(this.state.dreamGoal, this.state.dreamCurr);
+        console.log(this.props.user); // shows up on update
         return(
             <div className="stats-main">
                 <div className="myDreams">
                     <h1>My Dreams</h1>
                     <div className="bar">
-                        <ProgressCircle 
-                            donationGoal={100}
-                            donationsReceived={75}
+                        <UserProgressCircle 
+                            donationGoal={isNaN(this.state.dreamGoal) ? 0 : this.state.dreamGoal}
+                            donationsReceived={isNaN(this.state.dreamCurr) ? 0 : this.state.dreamCurr}
                         />
                       </div>
                     {/* <button>more info</button> */}
@@ -26,10 +62,10 @@ class Stats extends React.Component {
                 <div className="support-dreams">
                     <h1>Dreams Supported</h1>
                     <div className="bar">
-                        <ProgressCircle 
+                        {/* <UserProgressCircle 
                             donationGoal={100}
-                            donationsReceived={75}
-                        />
+                            donationsReceived={100}
+                        /> */}
                         </div>
                     {/* <button>more info</button> */}
                 </div>
@@ -38,4 +74,10 @@ class Stats extends React.Component {
     }
 }
 
-export default Stats;
+const mapStateToProps = ({ auth }) => {
+    return {
+      user: auth.user,
+    }
+  }
+
+export default connect(mapStateToProps, { })(Stats);
