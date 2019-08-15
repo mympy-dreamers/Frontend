a1fb57b7cb1ebcdbe7591f81c71a0ede919b5f6b
Manju KC <Manju KC>
2019-08-15 14:07:44 -0400

    changed the styling of inputModel

--- mympy-app/src/components/dreamForms/inputModal.js
+++ mympy-app/src/components/dreamForms/inputModal.js
@@ -2,6 +2,22 @@ import React from 'react';
 import {Modal, ModalHeader, ModalBody, ModalFooter, Button } from 'reactstrap';
 
 
+const bodyStyle = {
+  fontSize: "25px",
+  height:"120px",
+  marginTop:"40px",
+  
+
+}
+
+const headStyle = {
+  color:"red",
+  fontSize: "22px",
+  color: '#1B5485',
+ 
+}
+
+
 
 class InputModal extends React.Component {
 
@@ -9,9 +25,9 @@ class InputModal extends React.Component {
     return (
       <div>
         <Modal isOpen={this.props.showModal}>
-        <ModalHeader>Warning!!</ModalHeader>
-          <ModalBody>
-           "Are you sure to save your dream?"
+        <ModalHeader style={ headStyle }>Warning!!</ModalHeader>
+          <ModalBody style={ bodyStyle}>
+           "Are you sure you want to save your dream?"
           </ModalBody>
           <ModalFooter>
               <Button onClick={this.props.closeModal}>Okey</Button>
