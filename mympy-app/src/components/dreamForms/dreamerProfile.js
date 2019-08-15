480d0402195cacb2bf051a174f52ecec874ef946
Nickolas Stricker <43568886+NickStrick@users.noreply.github.com>
2019-08-15 17:43:52 -0500

    refactored submit functions

--- mympy-app/src/components/dreamForms/dreamerProfile.js
+++ mympy-app/src/components/dreamForms/dreamerProfile.js
@@ -17,10 +17,10 @@ class DreamInfo extends React.Component {
                 donations_received: 0,
                 donation_goal: 0,
                 user_id: null,
-               
-                
+
+
             },
-            showModal:false
+            showModal: false
         };
     }
 
@@ -42,21 +42,30 @@ class DreamInfo extends React.Component {
                 ...this.state.dreamCard,
                 [e.target.name]: e.target.value
             },
-           
+
+        })
+    }
+    handleFinalSubmit = e => {
+
+        const newDream = { ...this.state.dreamCard, donation_goal: parseInt(this.state.dreamCard.donation_goal, 10) }
+        this.props.addDream(newDream)
+        this.setState({ showModal: false }, () => {
+            this.props.history.push('/addDream/image')
+
         })
     }
 
     handleSubmit = e => {
-        const newDream = { ...this.state.dreamCard, donation_goal: parseInt(this.state.dreamCard.donation_goal, 10) }
+
         e.preventDefault()
         this.setState({
-            showModal:true 
-        }, () => this.props.addDream(newDream))
-        
+            showModal: true
+        })
+
     }
 
     closeModal = () => {
-        this.setState({showModal: false}, () => this.props.history.push('/addDream/image'))
+        this.setState({ showModal: false })
     }
 
     render() {
@@ -73,24 +82,24 @@ class DreamInfo extends React.Component {
                             <Input className="input-style" onChange={this.handleChanges} name="dream_name" id="dream_name" placeholder="Enter your dream here" />
                         </FormGroup>
                         <FormGroup>
-                            <Label className="dreamlable"  for="examplePassword">Donation Goals</Label>
+                            <Label className="dreamlable" for="examplePassword">Donation Goals</Label>
                             <Input className="input-style" onChange={this.handleChanges} name="donation_goal" id="donation_goal" placeholder="Enter Donation goal here" />
                         </FormGroup>
                         <FormGroup>
-                            <Label  className="dreamlable"  className="dreamlable" for="exampleText">Give us a short description</Label>
-                            <Input className="input-style"  onChange={this.handleChanges} type="textarea" name="dream_short_description" id="dream_short_description" />
+                            <Label className="dreamlable" className="dreamlable" for="exampleText">Give us a short description</Label>
+                            <Input className="input-style" onChange={this.handleChanges} type="textarea" name="dream_short_description" id="dream_short_description" />
                         </FormGroup>
                         <FormGroup>
-                            <Label  className="dreamlable" for="exampleText">Give us a long descrption</Label>
-                            <Input  className="input-style" onChange={this.handleChanges} type="textarea" name="dream_long_description" id="dream_long_description" />
+                            <Label className="dreamlable" for="exampleText">Give us a long descrption</Label>
+                            <Input className="input-style" onChange={this.handleChanges} type="textarea" name="dream_long_description" id="dream_long_description" />
                         </FormGroup>
                         <button className="dreambutton" onClick={this.handleSubmit}>Submit</button>
 
                     </div>
-                    {<InputModal showModal={this.state.showModal} closeModal={this.closeModal}/>}
-                    
+                    {<InputModal handleFinalSubmit={this.handleFinalSubmit} showModal={this.state.showModal} closeModal={this.closeModal} />}
+
                 </div>  {/* dreamer-card-app end  */}
-               
+
             </div> /* dream-Home-Page end */
         )
     }
