dfde57fe06758ecc21258cb2b8a0d11b9ea97741
Nickolas Stricker <43568886+NickStrick@users.noreply.github.com>
2019-08-19 17:59:45 -0500

    added classnames / id's to homepage sections

--- mympy-app/src/view/supporter/Supporter.js
+++ mympy-app/src/view/supporter/Supporter.js
@@ -4,16 +4,16 @@ import cx from '../../img/janetwide.png';
 
 const Supporter = () => {
     return (
-        <div className="support-main">
+        <div className="support-main" id="support-main">
             <div className="top">
                 <div>
                     <h1>Be a Supporter</h1>
                 </div>
-                <p>Poster can be one of the effective marketing and advertising materials. 
-                    It is also a great tool to use when you want to present your services to a 
-                    new league or as a point of sale display on picture day. The poster must show 
-                    your products that are available, along with your company name and contact information. 
-                    If you having trouble about creating a good 4 color poster print, a 4 color printing 
+                <p>Poster can be one of the effective marketing and advertising materials.
+                    It is also a great tool to use when you want to present your services to a
+                    new league or as a point of sale display on picture day. The poster must show
+                    your products that are available, along with your company name and contact information.
+                    If you having trouble about creating a good 4 color poster print, a 4 color printing
                     company that has 4 color poster printing service may be able to help you made your poster.
                 </p>
             </div>
@@ -24,8 +24,8 @@ const Supporter = () => {
                     </div>
                     <div className="info">
                         <h1>Full Name</h1>
-                        <p>Poster can be one of the effective marketing and advertising materials. 
-                        It is also a great tool to use when you want to present your services to a 
+                        <p>Poster can be one of the effective marketing and advertising materials.
+                        It is also a great tool to use when you want to present your services to a
                         new league or as a point of sale display on picture day.</p>
                     </div>
                 </div>
