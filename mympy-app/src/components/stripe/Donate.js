0b47904538b25ed74fb08f52d8fb037c2ed58528
Krisli Dimo <krislidimo@gmail.com>
2019-08-20 23:36:15 -0500

    added basic jsx structure and passed in donationTotal to CheckoutForm
    Co-authored-by: Dakotah-Hitze <dakotahhitze@gmail.com>
    Co-authored-by: Jared-Parrish <jarparrish@gmail.com>

--- mympy-app/src/components/stripe/Donate.js
+++ mympy-app/src/components/stripe/Donate.js
@@ -7,20 +7,44 @@ class Donate extends React.Component {
     constructor(props){
         super(props);
         this.state = {
-
+            donationAmount: 0,
+            mympyDonation: 0,
+            donationTotal: 0
         }
     }
 
     render(){
         return(
             <div>
-                <h1>Donate here!</h1>
+                <h1>Donate to</h1>
+                <h1>Project {this.props.given_name +' '+ this.props.family_name}</h1>
+                <div className='donation-amounts'>
+                    <h2>$10</h2>
+                    <h2>$15</h2>
+                    <h2>$20</h2>
+                    <h2>$25</h2>
+                </div>
+                <div className='custom-amount'>
+                    <p>Custom Amount</p>
+                    <input></input>
+                </div>
+                <div>
+                    <div><img src=''/>{}</div>
+                    <div>Help with Mympy's cost</div>
+                </div>
+                <button>total {this.state.donationTotal}</button>
                 <Elements>
-                    <CheckoutForm />
+                    <CheckoutForm donationTotal={this.state.donationTotal}/>
                 </Elements>
             </div>
         )
     }
+}
+
+const mapStateToProps = ({ user }) => {
+  return {
+    user: user.authZeroUser
+  }
 }
 
 export default Donate;
