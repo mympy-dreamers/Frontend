2beddbf8ff12cd46bb3641ddc7c82a70d733598b
HitzeD <d-hitze@comcast.net>
2019-08-21 12:48:20 -0400

    controlled form

--- mympy-app/src/components/stripe/Donate.js
+++ mympy-app/src/components/stripe/Donate.js
@@ -13,23 +13,40 @@ class Donate extends React.Component {
         }
     }
 
+    donationHandler = e => {
+        console.log(e.target.value);
+        this.setState({
+            ...this.state,
+            donationAmount: e.target.value,
+            donationTotal: Number(this.state.donationTotal) + Number(e.target.value),
+        })
+    }
+
+    customHandler = e => {
+        this.setState({
+            ...this.state,
+            mympyDonation: e.target.value,
+            donationTotal: Number(this.state.donationTotal) + Number(e.target.value),
+        })
+    }
+
     render(){
         return(
             <div>
                 <h1>Donate to</h1>
                 <h1>Project {this.props.given_name +' '+ this.props.family_name}</h1>
                 <div className='donation-amounts'>
-                    <h2>$10</h2>
-                    <h2>$15</h2>
-                    <h2>$20</h2>
-                    <h2>$25</h2>
+                    <button onClick={this.donationHandler} value={10}>$10</button> {/* value is in cents */}
+                    <button onClick={this.donationHandler} value={15}>$15</button>
+                    <button onClick={this.donationHandler} value={20}>$20</button>
+                    <button onClick={this.donationHandler} value={25}>$25</button>
                 </div>
                 <div className='custom-amount'>
                     <p>Custom Amount</p>
-                    <input></input>
+                    <input onChange={this.customHandler} value={this.state.mympyDonation} />
                 </div>
                 <div>
-                    <div><img src=''/>{}</div>
+                    <div><img src='#' alt='' />{}</div>
                     <div>Help with Mympy's cost</div>
                 </div>
                 <button>total {this.state.donationTotal}</button>
