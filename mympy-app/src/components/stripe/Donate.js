c9be63128c7947ac485400bccadae5815df2f0eb
Krisli Dimo <krislidimo@gmail.com>
2019-08-22 02:04:19 -0500

    added styling | modified custom amount functionality

--- mympy-app/src/components/stripe/Donate.js
+++ mympy-app/src/components/stripe/Donate.js
@@ -4,14 +4,19 @@ import { Elements } from 'react-stripe-elements';
 import CheckoutForm from './CheckoutForm';
 import styled from 'styled-components';
 
+import mini_logo from '../../img/mympy-logo-mini.PNG'
+
 const StyledDonate = styled.div`
+    display: flex;
+    flex-direction: column;
     max-width: 400px;
-    margin: 0 auto;
-    margin-top: 8vh;
+    margin: 8vh auto 8vh;
     padding: 1%;
-    border: 1px solid black;
+    // border: 1px solid black;
+
 
     .title {
+        align-self: baseline;
         background: black;
         color: white; 
         font-size: 24px;
@@ -28,47 +33,78 @@ const StyledDonate = styled.div`
     .donation-buttons {
         display: flex;
         justify-content: space-between;
-        margin-bottom: 24px;
+        margin-bottom: 18px;
 
-    }
-
-    .button {
-        width: 7em;
-        height: 7em;
-        background: white;
-        font-weight: bold;
-        border: 0.6px solid black;
-        border-radius: 14%;
-        outline: none;
-        margin: 0;
-        padding: 0;
-
-        h4  {
+        .button {
+            width: 2.1em;
+            height: 2.1em;
+            background: white;
+            font-weight: bold;
+            border: 0.8px solid black;
+            border-radius: 14%;
+            outline: none;
+            margin: 0;
+            padding: 0;
             font-size: 3.3em;    
             font-weight: bold;
         }
+
+        .active {
+            border: 4px solid black;
+        }
     }
 
-    .active {
-        border: 4px solid black;
-    }
 
     .custom-amount {
         display: flex;
         justify-content: space-between;
+        align-items: center;
         margin: 0;
-        margin-bottom: 20px;
+        margin-bottom: 3.2em;
 
         .custom-title {
             font-size: 26px;    
             font-weight: bold;    
         }
 
-        .input {
-            height: 4em;
+        .custom-input {
+            width: 5.6em;
+            height: 1.4em;
+            font-size: 3em;
+            text-align: right;
+            padding-right: 8px;
+            margin: 0;
             border: 1px solid black;
             border-radius: 6px;
+            outline: none;
+
+            &::-webkit-inner-spin-button {
+                appearance: none;
+            }
         }
+    }
+
+    .mympy-donation {
+        display: flex;
+        justify-content: flex-end;
+        margin-bottom: 0.4em;
+
+        img { height: 3.4em; }
+
+        h3 { 
+            font-size: 3em; 
+            font-weight: bold; 
+            margin-left: 0.4em;
+            margin-right: 0.2em;
+        }
+
+    }
+
+    .mympy-cost-msg {
+        font-size: 2em; 
+        text-align: right;
+        margin-right: 0.3em;
+        margin-bottom: 2em;
     }
 `
 
@@ -84,12 +120,12 @@ class Donate extends React.Component {
 
     donationHandler = e => {
         let classes = e.target.className;
-        e.target.className = classes.includes('active') ? classes.replace(' active', '') : classes+' active';
+        e.target.className = (classes === 'button') ? classes+' active' : classes.replace(' active', '');
         this.setState({
             ...this.state,
             donationAmount: e.target.value,
             donationTotal: Number(this.state.donationTotal) + (
-                e.target.className.includes('active') ? 
+                e.target.className.includes('button active') ? 
                 Number(e.target.value):
                 Number(-e.target.value)
             ),
@@ -97,11 +133,10 @@ class Donate extends React.Component {
     }
 
     customHandler = e => {
+        let customAmount = e.target.value;
         this.setState({
             ...this.state,
-            mympyDonation: e.target.value,
-            donationTotal: Number(this.state.donationTotal) + Number(e.target.value),
-
+            donationTotal: Number(customAmount) + Number(this.state.mympyDonation)
         })
     }
 
@@ -111,20 +146,24 @@ class Donate extends React.Component {
                 <h1 className='title'>Donate to</h1>
                 <h1 className='title name'>Project {this.props.given_name +' '+ this.props.family_name}</h1>
                 <div className='donation-buttons'>
-                    <button className='button' onClick={this.donationHandler} value={10}><h4>$10</h4></button>
-                    <button className='button' onClick={this.donationHandler} value={15}><h4>$15</h4></button>
-                    <button className='button' onClick={this.donationHandler} value={20}><h4>$20</h4></button>
-                    <button className='button' onClick={this.donationHandler} value={25}><h4>$25</h4></button>
+                    <button className='button' onClick={this.donationHandler} value={10}>$10</button>
+                    <button className='button' onClick={this.donationHandler} value={15}>$15</button>
+                    <button className='button' onClick={this.donationHandler} value={20}>$20</button>
+                    <button className='button' onClick={this.donationHandler} value={25}>$25</button>
                 </div>
                 <div className='custom-amount'>
                     <h5 className='custom-title'>Custom Amount</h5>
-                    <input className='input' placeholder='$0.00'></input>
+                    <input className='custom-input' 
+                        type='number' 
+                        step="1" 
+                        placeholder='$0'
+                        onChange={this.customHandler}
+                    />
                 </div>
                 <div>
-                    <div><img src=''/>{this.state.mympyDonation}</div>
-                    <div>Help with Mympy's cost</div>
+                    <div className='mympy-donation'><img src={mini_logo}/><h3>{'$' + this.state.mympyDonation +'.00'}</h3></div>
+                    <div className='mympy-cost-msg'>Help with Mympy's cost</div>
                 </div>
-                <button>total {this.state.donationTotal}</button>
                 <Elements>
                     <CheckoutForm donationTotal={this.state.donationTotal}/>
                 </Elements>
