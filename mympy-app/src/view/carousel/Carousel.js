8e41969e5f74692ff08a7c454eae598829e3cdde
Nickolas Stricker <43568886+NickStrick@users.noreply.github.com>
2019-08-22 16:18:31 -0500

    fixed warnings in homepage carousel

--- mympy-app/src/view/carousel/Carousel.js
+++ mympy-app/src/view/carousel/Carousel.js
@@ -1,8 +1,5 @@
 import React from 'react';
-
 import bigImage from '../../img/Galaxyvr/galaxyvr2x.png';
-import { Link } from 'react-router-dom';
-
 
 const Carousel = (props) => {
     console.log(props)
