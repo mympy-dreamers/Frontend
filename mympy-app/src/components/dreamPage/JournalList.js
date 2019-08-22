edd8d0c35a47d377399287807b257f821e2d29e1
Mylynh Nguyen <34496865+nmylynh@users.noreply.github.com>
2019-08-21 18:59:06 -0700

    dynamically rendered delete button

--- mympy-app/src/components/dreamPage/JournalList.js
+++ mympy-app/src/components/dreamPage/JournalList.js
@@ -2,25 +2,24 @@ import React, { Component } from 'react';
 import Accordion from 'react-bootstrap/Accordion';
 import { Button, Card } from 'react-bootstrap';
 class JournalList extends Component {
-  state = {
-  }
-
   render() {
     return (
       <Card>
 						<Card.Header>
-						<Accordion.Toggle as={Button} variant="link" eventKey={this.props.id}>
-							{this.props.title}
-						</Accordion.Toggle> 
-            <div className="pull-right">
-              {this.props.posted_on}
-              <button 
-              className="delete-button" 
-              onClick={()=>
-              window.confirm("Are you sure you wish to delete this item?") &&
-              this.props.deleteJournal(this.props.id)}>Delete
-            </button>
-            </div>
+                <Accordion.Toggle as={Button} variant="link" eventKey={this.props.id}>
+                  {this.props.title}
+                </Accordion.Toggle> 
+                <div className="pull-right">
+                  {this.props.posted_on}
+                  {this.props.currentUser.id === this.props.user_id 
+                  ? <button 
+                      className="delete-button" 
+                      onClick={()=>
+                      window.confirm("Are you sure you wish to delete this item?") &&
+                      this.props.deleteJournal(this.props.id)}>Delete
+                    </button>
+                  : null}
+                </div>
 						</Card.Header>
 						<Accordion.Collapse eventKey={this.props.id}>
 						<Card.Body>{this.props.body}</Card.Body>
