6eb59543537869d9345d106cf34d48e5c315b668
tashi woeser <tashiwoeser@tashis-MacBook-Pro.local>
2019-08-23 08:50:31 -0700

    styling commited

--- mympy-app/src/components/dreamPage/JournalForm.js
+++ mympy-app/src/components/dreamPage/JournalForm.js
@@ -3,21 +3,66 @@ import {Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
 import './journaList.css';
 import { connect } from "react-redux";
 import { addJournal } from '../../actions';
+import axios from "axios";
 
 const bodyStyle = {
   fontSize: "25px",
-  height:"120px",
+  height:"440px",
   marginTop:"40px",
-  width: '200px'
-
 }
 
 const headStyle = {
   fontSize: "30px",
   color: '#1B5485',
-  marginLeft: '150px'
- 
+  paddingLeft: '145px',
+
 }
+
+const journalForm = {
+  fontSize: "25px",
+  width: '120%',
+  paddingLeft: '70px',
+  color: '#1B5485',
+}
+
+const h1 = {
+  marginBottom: '15px',
+  paddingLeft: '100px'
+}
+
+const h2 = {
+  marginBottom: '15px',
+  paddingLeft: '60px'
+}
+
+const inputStyle1 = {
+  height: '70px',
+  width: '325px',
+  marginBottom: '30px',
+  borderRadius: '5px'
+}
+
+const inputStyle2 = {
+  height: '150px',
+  width: '325px',
+  marginBottom: '30px',
+  borderRadius: '5px'
+}
+
+const journalSubmitButton = {
+    height: '40px',
+    width: '120px',
+    borderRadius: '5rem',
+    fontSize: '20px',
+    marginLeft: '100px',
+    backgroundColor: '#A1F85C',
+    marginBottom: '20px'
+}
+
+const journalFormStyle = {
+  width: '120%'
+}
+
 
 class FormModal extends React.Component {
   constructor() {
@@ -27,7 +72,7 @@ class FormModal extends React.Component {
       journal: {
         title: '',
         body: '',
-        user_id: null,
+        dream_id: null,
       }
     };
   }
@@ -50,35 +95,41 @@ handleSubmit = e => {
   this.setState({
       modal: true
   })
-
 }
 
+
+
 handleFinalSubmit = e => {
-
-  const newJournal = { ...this.state.journal,}
+  const newJournal = { ...this.state.journal }
   // e.preventDefault()
-  console.log('submittedJournal: ', newJournal);
-  this.props.addJournal(newJournal)
+  console.log('submittedDream: ', newJournal);
+  this.props.addDream(newJournal)
   this.setState({ showModal: false }, () => {
-      this.props.history.push('/')
-
+      this.props.history.push(this.state.body)
   })
 }
 
+async componentDidMount() {
+  try { let res = await axios.get('http://localhost:5000/market/');
+      this.setState({journal: res.data})
+    console.log(this.state.journal)} catch(err){
+      console.error(err)
+      }
+  }
 
   render() {
     return (
-      <div>
+      <div style={journalFormStyle}>
         <Modal isOpen={this.state.modal}>
         <ModalHeader style={ headStyle } >Create A Journal</ModalHeader>
           <ModalBody style={ bodyStyle }>
           <div>
-          <form>
-                  <h1>Journal Title</h1>
-                  <input name='title' id='title' onChange={this.handleChanges}></input>
-                  <h1>Share Your Thoughts</h1>
-                  <input name='body' id='body' onChange={this.handleChanges}></input> 
-                  <button onClick={this.handleFinalSubmit}>Submit</button>
+          <form onSubmit style={journalForm}>
+                  <h1 style={h1}>Journal Title</h1>
+                  <input style={inputStyle1} name='title' id='title' onChange={this.handleChanges}></input>
+                  <h1 style={h2}>Share Your Thoughts</h1>
+                  <input style={inputStyle2} name='body' id='body' onChange={this.handleChanges}></input> 
+                  <button style={journalSubmitButton} onClick={this.handleFinalSubmit}>Submit</button>
            </form>
           </div>
            
