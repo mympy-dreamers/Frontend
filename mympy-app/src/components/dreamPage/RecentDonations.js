251a6d7a7cbc1d6bb2fce9ac199db3ec57120e1a
Krisli Dimo <krislidimo@gmail.com>
2019-08-27 18:12:11 -0500

    removed console.logs

--- mympy-app/src/components/dreamPage/RecentDonations.js
+++ mympy-app/src/components/dreamPage/RecentDonations.js
@@ -8,29 +8,27 @@ import { dreamPayFetch } from '../../actions/stripe.js';
 class RecentDonations extends React.Component {
 
 	componentDidMount() {
-		console.log('****************HERE****************');
 		this.props.dreamPayFetch(this.props.currDream.id);
-		console.log('****************HERE****************');
-		console.log(this.props.dreamPayments);
 	}
 
 	render() {
-		console.log('*****HERE******' + this.props.currDream.id);
 		return (
-			<div className="recent-donations">
-				<h2 className="header">Recent Donations</h2>
-	  		
-	  		<div className="donations-container">
-					{this.props.dreamPayments.slice(0,5).map( donation => {
-						return <div className="donation" key={donation.id}>
-							<img className="user-img" src={donation.img_url} alt="" />
-							<div className="donations-name-wrapper">
-								<h2 className="amount">{"$" + donation.donation_amount}</h2>
-								<p className="name">{"by " + donation.user_name.split(' ')[0]}</p>
+			<div>
+				{this.props.dreamPayments[0] && <div className="recent-donations">
+					<h2 className="header">Recent Donations</h2>
+		  		
+		  		<div className="donations-container">
+						{this.props.dreamPayments.slice(0,5).map( donation => {
+							return <div className="donation" key={donation.id}>
+								<img className="user-img" src={donation.img_url} alt="" />
+								<div className="donations-name-wrapper">
+									<h2 className="amount">{"$" + donation.donation_amount}</h2>
+									<p className="name">{"by " + donation.user_name.split(' ')[0]}</p>
+								</div>
 							</div>
-						</div>
-					})}
-	  		</div>
+						})}
+		  		</div>
+		   	</div>}
 	   	</div>
 		)
 	}
