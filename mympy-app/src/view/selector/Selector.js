6728db2e780877ace69b2a2cc4cbe31cbf77337a
HitzeD <d-hitze@comcast.net>
2019-08-29 16:30:34 -0400

    comments

--- mympy-app/src/view/selector/Selector.js
+++ mympy-app/src/view/selector/Selector.js
@@ -1,20 +1,34 @@
 import React from 'react';
 
-
+// stateless component holding the buttons
 const Selector = () => {
+
     return (
+
         <div className="select-main">
+
             <div className="left">
+
                 <button>DREAMER</button>
+
             </div>
+
             <div className="offset">
+
                 <h1>ARE YOU A</h1>
+
             </div>
+
             <div className="right">
+
                 <button>SUPPORTER</button>
+
             </div>
+
         </div>
+
     )
+
 }
 
 export default Selector;
