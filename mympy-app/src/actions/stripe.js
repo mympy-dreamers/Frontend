deee705be3491973dcc866e1efe14f1401d85d19
HitzeD <d-hitze@comcast.net>
2019-08-29 14:35:00 -0400

    comment file

--- mympy-app/src/actions/stripe.js
+++ mympy-app/src/actions/stripe.js
@@ -1,7 +1,5 @@
 import axios from 'axios';
 
-
-// const BASE_URL = 'https://mympy-dreamers-staging.herokuapp.com';
 const BASE_URL = process.env.REACT_APP_BE_URL || 'http://localhost:5000';
 
 // Dream specific endpoints for payments below
@@ -10,6 +8,8 @@ export const DREAM_PAY_START = 'DREAM_PAY_START';
 export const DREAM_PAY_SUCCESS = 'DREAM_PAY_SUCCESS';
 export const DREAM_PAY_FAILURE = 'DREAM_PAY_FAILURE';
 export const DONO_FETCH = 'DONO_FETCH';
+
+    // DREAM_PAY_* = GET operation
 
 export const dreamPayFetch = (id) => dispatch => {
     dispatch({ type: DREAM_PAY_START });
@@ -33,6 +33,8 @@ export const dreamPayFetch = (id) => dispatch => {
 export const DREAMPAY_POST_START = 'DREAMPAY_POST_START';
 export const DREAMPAY_POST_SUCCESS = 'DREAMPAY_POST_SUCCESS';
 export const DREAMPAY_POST_FAILURE = 'DREAMPAY_POST_FAILURE';
+
+// DREAMPAY_POST_* = POST operation
 
 export const dreamPayPost = (data) => dispatch => {
     dispatch({ type: DREAMPAY_POST_START });
@@ -58,6 +60,8 @@ export const USER_PAY_START = 'USER_PAY_START';
 export const USER_PAY_SUCCESS = 'USER_PAY_SUCCESS';
 export const USER_PAY_FAILURE = 'USER_PAY_FAILURE';
 
+// USER_PAY_* = GET operation
+
 export const userPayFetch = (id) => dispatch => {
     dispatch({ type: USER_PAY_START });
 
@@ -80,6 +84,8 @@ export const userPayFetch = (id) => dispatch => {
 export const USERPAY_POST_START = 'USERPAY_POST_START';
 export const USERPAY_POST_SUCCESS = 'USERPAY_POST_SUCCESS';
 export const USERPAY_POST_FAILURE = 'USERPAY_POST_FAILURE';
+
+// USERPAY_POST* = POST operation
 
 export const userPayPost = (data) => dispatch => {
     dispatch({ type: USERPAY_POST_START });
