8e0d90ad18d3a48b826ad2a1155e17d6a34832f1
Krisli Dimo <krislidimo@gmail.com>
2019-08-29 02:46:55 -0500

    added tab with clickHandler to navBar

--- mympy-app/src/components/userBar/UserBar.js
+++ mympy-app/src/components/userBar/UserBar.js
@@ -18,11 +18,12 @@ class UserBar extends React.Component {
     }
 
     clickHandler = e => {
-        console.log(document.querySelector('.nav-tab-wrapper'))
-        // const classes = document.querySelector('.nav-tab-wrapper').className
-        // classes.inludes(' active') ?
-        // classes.replace(' active', '') :
-        // (classes += ' active')
+        let classes = document.querySelector('.nav-tab-wrapper').className;
+        document.querySelector('.nav-tab-wrapper').className = (
+            (classes === 'nav-tab-wrapper') ? 
+            (classes + ' active') : 
+            classes.replace(' active', '')
+        )
     }
 
     render() {
@@ -30,46 +31,43 @@ class UserBar extends React.Component {
             <div className="user-main">
                 <div className="nav-tab-wrapper" >
                     <div className="user-left">
-                        <h3>Hello {this.props.authZeroUser.given_name.toUpperCase() + " " + this.props.authZeroUser.family_name.toUpperCase()}!</h3>
                         <img src={this.props.authZeroUser.picture} alt='user' />
+                        <h3>Hello, {this.props.authZeroUser.given_name.toUpperCase() + " " + this.props.authZeroUser.family_name.toUpperCase()}!</h3>
                     </div>
-                    <div className="flexing">
+                    <div className="sub-nav">
                         <Link to="/addDream">
-                            <div className="user-tab">
+                            <div className="nav-icon" onClick={this.clickHandler}>
                                 <img src={cloudPlus} alt='cloud with a plus sign' />
                                 <p>Create Dream</p>
                             </div>
                         </Link>
                         <Link to="/user-dreams">
-                            <div className="user-tab">
+                            <div className="nav-icon" onClick={this.clickHandler}>
                                 <img src={cloudPencil} alt='cloud with pencil inside' />
                                 <p>Edit Dreams</p>
                             </div>
                         </Link>
 
-                        <div className="user-tab develop">
+                        <div className="nav-icon develop">
                             <img src={journal} alt='pencil scribbling' />
                             <p>Journal</p>
-                            <h6>(Under Development)</h6>
                         </div>
 
                         {/* <Link to="#"> */}
-                        <div className="user-tab develop">
+                        <div className="nav-icon develop">
                             <img src={messageLogo} alt='message logo' />
                             <p>Messages</p>
-                            <h6>(Under Development)</h6>
                         </div>
                         {/* </Link> */}
                         {/* <Link to="#"> */}
-                        <div className="user-tab develop">
+                        <div className="nav-icon develop">
                             <img src={userLogo} alt='person with a pencil' />
                             <p>Edit Bio</p>
-                            <h6>(Under Development)</h6>
                         </div>
                         {/* </Link> */}
                     </div>
                 </div>
-                <div className="arrow" onClick={this.clickHandler()} >
+                <div className="arrow-tab" onClick={this.clickHandler} >
                     <img src={arrow} alt='' />
                 </div>
             </div>
