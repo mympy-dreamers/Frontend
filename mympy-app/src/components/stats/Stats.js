c4ea59f18f6ddcc8407f4a58bd4593a78c0608a4
HitzeD <d-hitze@comcast.net>
2019-08-29 16:30:09 -0400

    comments

--- mympy-app/src/components/stats/Stats.js
+++ mympy-app/src/components/stats/Stats.js
@@ -15,32 +15,58 @@ class Stats extends React.Component {
     }
 
     componentDidMount() {
+
+        // functions goes and retreives the information pertaining to the user about dreams they have funded
+
         this.props.dreamPayFetch(this.props.userId);
+
     }
 
     render() {
+
         return(
+
             <div className="stats-main">
+
                 <div className="myDreams">
+
                     <h1>My Dreams</h1>
+
                     <div className="bar">
+
                         <UserProgressCircle 
                             donationGoal={this.props.userGoal}
                             donationsReceived={this.props.receivedDonations}
                         />
+
                     </div>
+                    
+                    {/* idea was for button to have a pop up showing a list and total of dream donations */}
                     {/* <button>more info</button> */}
+
                 </div>
+
                 <div className="support-dreams">
+
                     <h1>Total Donations</h1>
+
                     <div className="bar total-donations">
+                            {/* should also be a data viz that did not get implemented as the proper information was not saved in the DB */}
                         ${this.props.receivedDonations}
+
                     </div>
+
+                    {/* idea was for button to have a pop up showing a list and total of dream donations */}
                     {/* <button>more info</button> */}
+
                 </div>
+
             </div>
+
         )
+
     }
+
 }
 
 const mapStateToProps = ({ auth, stripe }) => {
